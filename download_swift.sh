f853bcf1147e492109a0e7dfd64eff042014f315
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 08:49:28 -0500

    Update and rename install_swift.sh to download_swift.sh

--- download_swift.sh
+++ download_swift.sh
@@ -28,9 +28,3 @@ git clone --single-branch -b main https://github.com/philipturner/swift-colab
 
 export PATH="/swift/toolchain/usr/bin:$PATH"
 swift swift-colab/install_swift.swift
-
-# cd /swift
-# curl https://raw.githubusercontent.com/philipturner/swift-colab/main/install_swift.swift --output install_swift.swift
-
-# export PATH="/swift/toolchain/usr/bin:$PATH"
-# swift install_swift.swift
