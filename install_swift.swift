e193c9a067b116ec775c0baf92e9ec4b6bfa7db6
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 11:46:57 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -7,25 +7,13 @@ defer { print("=== Swift successfully installed ===") }
 let fm = FileManager.default
 precondition(fm.currentDirectoryPath == "/swift", "Called `install_swift.swift` when the working directory was not `/swift`.")
 
+// Remove any previously existing `run_swift` files
 try fm.removeItem(atPath: "/swift/run_swift.sh")
 try fm.removeItem(atPath: "/swift/run_swift.swift")
 
+// Move `run_swift` to the `/swift` directory
 try fm.moveItem(atPath: "/swift/swift-colab/run_swift.sh", toPath: "/swift/run_swift.sh")
 try fm.moveItem(atPath: "/swift/swift-colab/run_swift.swift", toPath: "/swift/run_swift.swift")
 
-// func moveToParent(fileName: String) throws {
-//     let targetPath = "/swift/\(fileName)"
-    
-// //     if fm.fileExists(atPath: targetPath) {
-//         try fm.removeItem(atPath: targetPath)
-// //     }
-    
-//     try fm.moveItem(atPath: "/swift/swift-colab/\(fileName)", toPath: targetPath)
-// }
-
-// try moveToParent(fileName: "run_swift.sh")
-// try moveToParent(fileName: "run_swift.swift")
-
-
-
+// Create directory for temporary files created while executing a Swift script
 try fm.createDirectory(atPath: "/swift/tmp", withIntermediateDirectories: true)
