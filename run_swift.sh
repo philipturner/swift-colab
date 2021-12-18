a891a73d663904bfd83174235190a6d00892027a
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 09:44:43 -0500

    Update run_swift.sh

--- run_swift.sh
+++ run_swift.sh
@@ -1,23 +1,23 @@
-if [[ $PATH == "/swift/toolchain/usr/bin"* ]]
+if [[ $PATH != "/swift/toolchain/usr/bin"* ]]
 then
-  echo "Path contained swift: $PATH"
-  echo $PATH
-else
   echo "Path did not contain swift 2"
   echo $PATH
   
   export PATH="/swift/toolchain/usr/bin:$PATH"
+else
+  echo "Path contained swift: $PATH"
+  echo $PATH
 fi
 
 echo "trying again"
 
-if [[ $PATH == "/swift/toolchain/usr/bin"* ]]
+if [[ $PATH != "/swift/toolchain/usr/bin"* ]]
 then
-  echo "Path contained swift: $PATH"
-  echo $PATH
-else
   echo "Path did not contain swift 2"
   echo $PATH
   
   export PATH="/swift/toolchain/usr/bin:$PATH"
+else
+  echo "Path contained swift: $PATH"
+  echo $PATH
 fi
