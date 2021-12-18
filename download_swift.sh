bc1514eeabd3cedb4b5b580fd6696bb0396192bb
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 09:30:39 -0500

    Update download_swift.sh

--- download_swift.sh
+++ download_swift.sh
@@ -2,26 +2,26 @@
 
 if [[ ! -d /swift ]]
 then
-    mkdir /swift
+  mkdir /swift
 fi
 
 cd /swift
 
 if [[ ! -d /swift/toolchain ]]
 then
-    curl https://download.swift.org/swift-5.5.2-release/ubuntu1804/swift-5.5.2-RELEASE/swift-5.5.2-RELEASE-ubuntu18.04.tar.gz \
-        --output toolchain-tar
-    
-    tar -xvzf toolchain-tar -C /swift
-    rm toolchain-tar
-    mv swift-5.5.2-RELEASE-ubuntu18.04 toolchain
+  curl https://download.swift.org/swift-5.5.2-release/ubuntu1804/swift-5.5.2-RELEASE/swift-5.5.2-RELEASE-ubuntu18.04.tar.gz \
+      --output toolchain-tar
+  
+  tar -xvzf toolchain-tar -C /swift
+  rm toolchain-tar
+  mv swift-5.5.2-RELEASE-ubuntu18.04 toolchain
 fi
 
 # Execute setup script
 
 if [[ -d /swift/swift-colab ]]
 then
-    rm -r /swift/swift-colab
+  rm -r /swift/swift-colab
 fi
 
 git clone --single-branch -b main https://github.com/philipturner/swift-colab
