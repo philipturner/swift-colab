0ac77dabf2877e318a771dd3f6ff4ed46f3ab866
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 07:48:16 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -11,9 +11,8 @@ curl https://download.swift.org/swift-5.5.2-release/ubuntu1804/swift-5.5.2-RELEA
 
 tar -xvzf toolchain-zipped -C /swift
 rm toolchain-zipped 
-echo "Hello world"
-echo $pwd
-echo pwd
+echo "Hello world 2"
+pwd
 mv swift-5.5.2-RELEASE-ubuntu18.04 toolchain
 
 if [[ ! -d /projects ]]
