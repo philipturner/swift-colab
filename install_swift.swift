eac887c23c146e66f558745eef080038734bccf3
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 10:47:33 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -3,22 +3,3 @@ import Foundation
 print()
 print("=== Swift successfully downloaded ===")
 defer { print("=== Swift successfully installed ===") }
-
-// Show environment
-
-let environment = ProcessInfo.processInfo.environment
-
-for key in environment.keys.sorted() {
-    print("Environment variable \(key) = \(environment[key]!)")
-}
-
-// Investigate the Python directory
-
-let fm = FileManager.default
-
-do {
-    let pathContents = try fm.contentsOfDirectory(atPath: "/usr")
-    print(pathContents)
-} catch {
-    print("Couldn't find path contents: \(error.localizedDescription)")
-}
