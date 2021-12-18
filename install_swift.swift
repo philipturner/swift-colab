e048ed11c44d005eadfa3328d4a66007799c3bb9
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 11:21:24 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -5,6 +5,9 @@ print("=== Swift successfully downloaded ===")
 defer { print("=== Swift successfully installed ===") }
 
 let fm = FileManager.default
+print("Working directory: \(fm.currentDirectoryPath)")
 
 try fm.moveItem(atPath: "/swift/swift-colab/run_swift.sh", toPath: "/swift/run_swift.sh")
 try fm.moveItem(atPath: "/swift/swift-colab/run_swift.swift", toPath: "/swift/run_swift.swift")
+
+try fm.createDirectory(atPath: "/swift/tmp", withIntermediateDirectories: true)
