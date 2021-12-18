4bfa1f0317fcba6e425845dd5110842efd2a6eab
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 07:22:09 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -1,14 +1,4 @@
-#mkdir /swift && cd /swift && curl https://download.swift.org/swift-5.5.2-release/ubuntu1804/swift-5.5.2-RELEASE/swift-5.5.2-RELEASE-ubuntu18.04.tar.gz --output toolchain-zipped
-
-#cd /swift && tar -xvzf toolchain-zipped -C /swift
-
-#cd /swift && rm toolchain-zipped && mv swift-5.5.2-RELEASE-ubuntu18.04 toolchain
-
-#mkdir /projects && cd /projects && mkdir Hello
-
-#export PATH="/swift/toolchain/usr/bin:$PATH" && cd /projects/Hello && swift package init && swift build
-
-##!/bin/bash
+#!/bin/bash
 
 mkdir /swift 
 cd /swift
