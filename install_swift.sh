b7a02bbd9a82d8d6fe38f3ef8cfb47fc132890e0
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 10:28:37 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -13,15 +13,9 @@ if [[ ! -d /swift/toolchain ]]
 then  
   ver="5.5.2"
   tar_file="swift-${ver}-RELEASE-ubuntu18.04"
-  url="https://download.swift.org/swift-${ver}-release/ubuntu1804/swift-${ver}-RELEASE/${tar_file}.tar.gz"
   
-  curl "${url}" | tar -xz
-#   curl "https://download.swift.org/swift-${ver}-release/ubuntu1804/swift-${ver}-RELEASE/${tar_file}.tar.gz" | tar -xz
+  curl "https://download.swift.org/swift-${ver}-release/ubuntu1804/swift-${ver}-RELEASE/${tar_file}.tar.gz" | tar -xz
   mv "${tar_file}" toolchain
-  
-#   curl https://download.swift.org/swift-5.5.2-release/ubuntu1804/swift-5.5.2-RELEASE/swift-5.5.2-RELEASE-ubuntu18.04.tar.gz \
-#     | tar -xz
-#   mv swift-5.5.2-RELEASE-ubuntu18.04 toolchain
 fi
 
 # Execute setup script
