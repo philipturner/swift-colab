98fb33a249727695afe16d820590c5ae08762600
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 11:38:35 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -7,18 +7,24 @@ defer { print("=== Swift successfully installed ===") }
 let fm = FileManager.default
 precondition(fm.currentDirectoryPath == "/swift", "Called `install_swift.swift` when the working directory was not `/swift`.")
 
-func moveToParent(fileName: String) throws {
-    let targetPath = "/swift/\(fileName)"
-    
-//     if fm.fileExists(atPath: targetPath) {
-        try fm.removeItem(atPath: targetPath)
-//     }
-    
-    try fm.moveItem(atPath: "/swift/swift-colab/\(fileName)", toPath: targetPath)
-}
+try fm.removeItem(atPath: "/swift/run_swift.sh")
+try fm.removeItem(atPath: "/swift/run_swift.swift")
 
-try moveToParent(fileName: "run_swift.sh")
-try moveToParent(fileName: "run_swift.swift")
+try fm.moveItem(atPath: "/swift/swift-colab/run_swift.sh", toPath: "/swift/run_swift.sh")
+try fm.moveItem(atPath: "/swift/swift-colab/run_swift.swift", toPath: "/swift/run_swift.swift")
+
+// func moveToParent(fileName: String) throws {
+//     let targetPath = "/swift/\(fileName)"
+    
+// //     if fm.fileExists(atPath: targetPath) {
+//         try fm.removeItem(atPath: targetPath)
+// //     }
+    
+//     try fm.moveItem(atPath: "/swift/swift-colab/\(fileName)", toPath: targetPath)
+// }
+
+// try moveToParent(fileName: "run_swift.sh")
+// try moveToParent(fileName: "run_swift.swift")
 
 
 
