d08302befe76b058604044e50504cb5daae3fb5b
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 15:00:06 -0500

    Update install_swift.py

--- Sources/SwiftColab/install_swift/install_swift.py
+++ Sources/SwiftColab/install_swift/install_swift.py
@@ -1,1 +1,2 @@
-# Register `swift` Python package
+# Register `swift` Python package - this will be transferred into Swift once I can import PythonKit
+import sys; sys.path.insert(0, "/env/python/swift")
