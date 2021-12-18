23b0acddd030c6f3f2f242a2d5568948c4c90e18
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 14:56:41 -0500

    Update install_swift.swift

--- Sources/SwiftColab/install_swift/install_swift.swift
+++ Sources/SwiftColab/install_swift/install_swift.swift
@@ -24,8 +24,9 @@ try fm.removeItemIfExists(atPath: "/swift/run_swift.sh")
 try fm.removeItemIfExists(atPath: "/swift/run_swift.swift")
 
 // Move `run_swift` to the `/swift` directory
-try fm.moveItem(atPath: "/swift/swift-colab/run_swift.sh", toPath: "/swift/run_swift.sh")
-try fm.moveItem(atPath: "/swift/swift-colab/run_swift.swift", toPath: "/swift/run_swift.swift")
+let baseDirectory = "/swift/swift-colab/Sources/SwiftColab/run_swift"
+try fm.moveItem(atPath: "\(baseDirectory)/run_swift.sh", toPath: "/swift/run_swift.sh")
+try fm.moveItem(atPath: "\(baseDirectory)/run_swift.swift", toPath: "/swift/run_swift.swift")
 
 // Create directory for temporary files created while executing a Swift script
 try fm.createDirectory(atPath: "/swift/tmp", withIntermediateDirectories: true)
@@ -33,14 +34,21 @@ try fm.createDirectory(atPath: "/swift/tmp", withIntermediateDirectories: true)
 // Add `swift` python module to `/env/python` directory
 try fm.createDirectory(atPath: "/env/python", withIntermediateDirectories: true)
 
-let sourceURL = URL(fileURLWithPath: "/swift/swift-colab/PythonPackages")
-let targetURL = URL(fileURLWithPath: "/env/python")
+let sourcePath = "/swift/swift-colab/PythonPackages/swift"
+let targetPath = "env/python/swift"
 
-for package in try fm.contentsOfDirectory(atPath: sourceURL.path) {
-    print("Registering Python package \"\(package)\"")
-    let packageSourceURL = sourceURL.appendingPathComponent(package)
-    let packageTargetURL = targetURL.appendingPathComponent(package)
+try fm.removeItemIfExists(atPath: targetPath)
+try fm.moveItem(atPath: sourcePath, toPath: targePath)
+
+
+// let sourceURL = URL(fileURLWithPath: "/swift/swift-colab/PythonPackages")
+// let targetURL = URL(fileURLWithPath: "/env/python")
+
+// for package in try fm.contentsOfDirectory(atPath: sourceURL.path) {
+//     print("Registering Python package \"\(package)\"")
+//     let packageSourceURL = sourceURL.appendingPathComponent(package)
+//     let packageTargetURL = targetURL.appendingPathComponent(package)
     
-    try fm.removeItemIfExists(atPath: packageTargetURL.path)
-    try fm.moveItem(at: packageSourceURL, to: packageTargetURL)
-}
+//     try fm.removeItemIfExists(atPath: packageTargetURL.path)
+//     try fm.moveItem(at: packageSourceURL, to: packageTargetURL)
+// }
