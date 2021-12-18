9cdd48dcab98bdcbba27cb0224a4255f3887adb2
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 08:57:28 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -2,7 +2,7 @@ import Foundation
 
 print()
 print("=== Swift successfully downloaded ===")
-defer { print("=== Swift successfully installed ===")
+defer { print("=== Swift successfully installed ===") }
 
 if let value = ProcessInfo.processInfo.environment["PATH"] {
     print("PATH is \(value)")
