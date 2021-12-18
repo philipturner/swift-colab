c02d9429c757189f13702af0b14156c1608915dc
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 12:30:29 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -17,4 +17,14 @@ try fm.moveItem(atPath: "/swift/swift-colab/run_swift.swift", toPath: "/swift/ru
 
 // Create directory for temporary files created while executing a Swift script
 try fm.createDirectory(atPath: "/swift/tmp", withIntermediateDirectories: true)
+
+// Add `swift` python module to global search path
 try fm.createDirectory(atPath: "/env/python", withIntermediateDirectories: true)
+try fm.removeItem(atPath: "/env/python/swift")
+// try fm.moveItem(atPath: "/swift/swift-colab/PythonPa
+
+print("Testing contentsOfDirectory method")
+print(try fm.contentsOfDirectory(atPath: "/swift/swift-colab/PythonPackages"))
+
+print("Testing subpaths method")
+print(try fm.subpaths(atPath: "/swift/swift-colab/PythonPackages"))
