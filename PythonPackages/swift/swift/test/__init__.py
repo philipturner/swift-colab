bfc2d68a88ba4f30fb93e22ee25abee691988f73
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 13:47:41 -0500

    Update __init__.py

--- PythonPackages/swift/swift/test/__init__.py
+++ PythonPackages/swift/swift/test/__init__.py
@@ -1,1 +1,19 @@
+import unittest
+from test import support
 
+class MyTestCase1(unittest.TestCase):
+
+    # Only use setUp() and tearDown() if necessary
+
+    def setUp(self):
+        
+
+    def tearDown(self):
+        
+
+    def test_feature_one(self):
+        # Test feature one.
+        print("hello world testing feature one")
+
+if __name__ == '__main__':
+    unittest.main()
