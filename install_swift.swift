e2ca87215e3df2d4fac5e5ea8621fcee56bb8933
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 08:59:10 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -4,8 +4,8 @@ print()
 print("=== Swift successfully downloaded ===")
 defer { print("=== Swift successfully installed ===") }
 
-if let value = ProcessInfo.processInfo.environment["PATH"] {
-    print("PATH is \(value)")
+if let value = ProcessInfo.processInfo.environment["PYTHONHOME"] {
+    print("PYTHONHOME is \(value)")
 } else {
-    print("PATH was not found")
+    print("PYTHONHOME was not found")
 }
