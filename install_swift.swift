ad4c08a296448f74b756eec0adc701a8b9fa2fd5
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 09:06:51 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -4,10 +4,16 @@ print()
 print("=== Swift successfully downloaded ===")
 defer { print("=== Swift successfully installed ===") }
 
+// Show environment
+
 let environment = ProcessInfo.processInfo.environment
 
 for key in environment.keys.sorted() {
     print("Environment variable \(key) = \(environment[key]!)")
 }
 
+// Investigate the Python directory
+
 let fm = FileManager.default
+
+print(fm.contentsOfDirectory(atPath: "/env/python"))
