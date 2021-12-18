ae009b2b4e465756f713a83347b2ae2a508e4c4e
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 11:15:18 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -3,3 +3,8 @@ import Foundation
 print()
 print("=== Swift successfully downloaded ===")
 defer { print("=== Swift successfully installed ===") }
+
+let fm = FileManager.default
+
+fm.moveItem(atPath: "/swift/swift-colab/run_swift.sh", to: "/swift/run_swift.sh")
+fm.moveItem(atPath: "/swift/swift-colab/run_swift.swift", to: "/swift/run_swift.swift")
