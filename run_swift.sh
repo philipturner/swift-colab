217d1b8ed5bf19eacde9a6768179e709de488f4c
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 09:32:19 -0500

    Update run_swift.sh

--- run_swift.sh
+++ run_swift.sh
@@ -1,1 +1,10 @@
+if [[ $PATH == "/swift/toolchain/usr/bin" ]]
+then
+  echo "Path contained swift"
+  echo $PATH
+else
+  echo "Path did not contain swift"
+  echo $PATH
+fi
 
+# export PATH="/swift/toolchain/usr/bin:$PATH"
