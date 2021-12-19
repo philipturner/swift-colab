d9cfda99c67af01c1cfc8e873568c53551606517
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 20:02:34 -0500

    Update setup.py

--- PythonPackages/swift/setup.py
+++ PythonPackages/swift/setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 # Setting up
 setup(
     # the name must match the folder name 'swift'
-    name="swift", 
-    version="1.0.0",
-    author="Philip Turner",
+    name="swift"
+#     version="1.0.0",
+#     author="Philip Turner",
 #     author_email="philipturner.AR@gmail.com",
 #     description="Execute Swift code from Python",
 #     long_description="Allows running Swift code as Python strings in Google Colab",
-    packages=find_packages()
+#     packages=find_packages()
 )
