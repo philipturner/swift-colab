5c91b80e3261b7e5783f23b0e2555874b0261cba
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 22:15:35 -0500

    Update run_swift.swift

--- Sources/SwiftColab/run_swift/run_swift.swift
+++ Sources/SwiftColab/run_swift/run_swift.swift
@@ -2,6 +2,18 @@ import Foundation
 print("=== Started running Swift string ===")
 defer { print("=== Finished running Swift string ===") }
 
+print("Arguments: \(CommandLine.arguments)")
+
+// Write the script to temporary file
+
+let scriptData = CommandLine.arguments[1].data(using: .utf8)
+let targetURL = URL(fileURLWithPath: "/opt/swift/tmp/string_script.swift")
+try scriptData.write(to: targetURL, options: .atomic)
+
+print("successfully wrote string script")
+
+// Execute the script
+
 FileManager.default.changeCurrentDirectoryPath("/contents")
 
-print("Arguments: \(CommandLine.arguments)")
+
