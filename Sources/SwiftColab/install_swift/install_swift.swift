85f07649c05cee46109ee8da9bd1ad2ddf3c76e5
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 19:46:19 -0500

    Update install_swift.swift

--- Sources/SwiftColab/install_swift/install_swift.swift
+++ Sources/SwiftColab/install_swift/install_swift.swift
@@ -28,53 +28,22 @@ let baseDirectory = "/swift/swift-colab/Sources/SwiftColab/run_swift"
 try fm.moveItem(atPath: "\(baseDirectory)/run_swift.sh", toPath: "/swift/run_swift.sh")
 try fm.moveItem(atPath: "\(baseDirectory)/run_swift.swift", toPath: "/swift/run_swift.swift")
 
-
-print("swift debug marker 0")
-
-
 // Create directory for temporary files created while executing a Swift script
 try fm.createDirectory(atPath: "/swift/tmp", withIntermediateDirectories: true)
-
-
-
-print("swift debug marker 1")
-
-
-
 
 // Add `swift` python module to `/env/python` directory
 try fm.createDirectory(atPath: "/env/python", withIntermediateDirectories: true)
 try fm.createDirectory(atPath: "/env/python/swift", withIntermediateDirectories: true)
 
-print("swift debug marker 2")
-
-
-
 let sourcePath = "/swift/swift-colab/PythonPackages/swift"
 let targetPath = "/env/python/swift"
 
 try fm.removeItemIfExists(atPath: targetPath)
-
-
-print("swift debug marker 3")
-
-
 try fm.moveItem(atPath: sourcePath, toPath: targetPath)
 
 
 
-print("swift debug marker 4")
+print("swift debug marker 0")
 
 
 
-// let sourceURL = URL(fileURLWithPath: "/swift/swift-colab/PythonPackages")
-// let targetURL = URL(fileURLWithPath: "/env/python")
-
-// for package in try fm.contentsOfDirectory(atPath: sourceURL.path) {
-//     print("Registering Python package \"\(package)\"")
-//     let packageSourceURL = sourceURL.appendingPathComponent(package)
-//     let packageTargetURL = targetURL.appendingPathComponent(package)
-    
-//     try fm.removeItemIfExists(atPath: packageTargetURL.path)
-//     try fm.moveItem(at: packageSourceURL, to: packageTargetURL)
-// }
