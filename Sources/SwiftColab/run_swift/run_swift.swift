46e1184f66922ebe527844b0eafc48dde4c09dbf
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 21:43:40 -0500

    Update run_swift.swift

--- Sources/SwiftColab/run_swift/run_swift.swift
+++ Sources/SwiftColab/run_swift/run_swift.swift
@@ -1,3 +1,7 @@
 import Foundation
 print("=== Running Swift string ===")
+defer { print("=== Finished running Swift string ===") }
+
+FileManager.default.currentDirectoryPath = "/contents"
+
 print("Arguments: \(CommandLine.arguments)")
