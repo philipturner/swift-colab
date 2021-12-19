7cc5bb3a8ad7707b49fe04226580f2c371a0bece
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 20:37:51 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -2,5 +2,5 @@ import subprocess
 
 def run(swift_string):
   print("starting task 22")
-  subprocess.run(["bash", "/swift/run_swift.sh", swift_string], capture_output=True, check=True, text=True)
+  subprocess.run(["bash", "/swift/run_swift.sh", "hello world 333"], capture_output=True, check=True, text=True)
   print("finishing task 22")
