ca8bda29658ff754f39fbc04e973d6c052bcd15d
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 21:29:27 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,7 +1,5 @@
 import os
 
 def run(swift_string): 
-  command_string =  f"bash /swift/run_swift.sh \"${swift_string}\""
-  print("command string is", command_string)
-  output = os.popen(f"bash /swift/run_swift.sh \"${swift_string}\"")
+  output = os.popen(f"bash /swift/run_swift.sh \"{swift_string}\"")
   print(output.read())
