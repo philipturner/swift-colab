1fb1a0809edc66c386ad9dd879ed764839297bc2
Philip Turner <philipturner.AR@gmail.com>
2021-12-19 08:31:23 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,3 +1,6 @@
 import subprocess as sp
 def run(swift_string):
-  print(sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.STDOUT, text=True).stdout)
+#   print(sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.STDOUT, text=True).stdout)
+    p = sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.PIPE, text=True)
+    print(p.stdout)
+    print(p.stderr)
