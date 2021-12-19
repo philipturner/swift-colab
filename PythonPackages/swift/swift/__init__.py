26751da685f9a590e282c03f1613004108debf23
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 20:28:12 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,5 +1,4 @@
 import subprocess
 
-def run(str1, str2):
-  print(str1, str2)
-#   subprocess.run(["bash", "/swift/run_swift.sh", str])
+def run(swift_string):
+  subprocess.run(["bash", "/swift/run_swift.sh", swift_string])
