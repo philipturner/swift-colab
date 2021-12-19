be22bc78c112ca294b3fa74e11a2bde1a14383a3
Philip Turner <philipturner.AR@gmail.com>
2021-12-18 21:31:27 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,5 +1,2 @@
-import os
-
-def run(swift_string): 
-  output = os.popen(f"bash /swift/run_swift.sh \"{swift_string}\"")
-  print(output.read())
+import os; def run(swift_string): 
+  output = os.popen(f"bash /swift/run_swift.sh \"{swift_string}\""); print(output.read())
