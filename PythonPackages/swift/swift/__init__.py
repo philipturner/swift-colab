9dc877dd7fb2680e20f995eb71c9ec9d35179d89
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 09:27:51 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,3 +1,4 @@
 import subprocess as sp
-def run(swift_string):
+import ctypes
+def run(swift_string): # will change to directly call a Swift dynamic library via C interop. This should let stdout and stderr be synchronized with the Jupyter kernel.
     p = sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.PIPE, text=True); print(p.stdout + p.stderr)
