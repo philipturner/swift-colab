0d2ac63d4876288503726495a413466f9dfe5de2
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 16:39:36 -0500

    Update SwiftPythonBridge.swift

--- Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
+++ Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
@@ -2,11 +2,8 @@
 // will import PythonKit eventually, but for now, use OpaquePointer instead of PythonObject
 // can also be imported from Swift, as it defines the function wrapper struct
 
-// 6 accepted Swift types;
-// @convention(c) (PythonObject) throws -> Void
-// @convention(c) (PythonObject) throws -> PythonObject
-// (PythonObject) throws -> Void
-// (PythonObject) throws -> PythonObject
+// 1 accepted Swift type:
+// (PythonObject) throws -> PythonObject?
 
 // Workaround that lets me avoid modifying PythonKit: make a class that replicates the behavior of PyReference. Then, unsafe bit cast it to a PythonObject.
 
