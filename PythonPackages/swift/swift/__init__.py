a76d9c09458e404a246f93dd866a7e4b9ab326ea
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 10:32:49 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,5 +1,4 @@
-import subprocess as sp
-import ctypes
+import subprocess as sp; import ctypes
 def run(swift_string): # will change to directly call a Swift dynamic library via C interop. This should let stdout and stderr be synchronized with the Jupyter kernel.
     p = sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.PIPE, text=True); print(p.stdout + p.stderr)
 class SwiftObject:
@@ -11,7 +10,10 @@ class SwiftObject:
     def call_swift_function(self, function_name, parameter):
         func_ptr_wrapper_ptr = c_void_p(self.function_table[function_name]) # cast an integer to an `OpaquePointer`
         func_return_ptr = self.__bridge_lib.callSwiftFromPython(c_void_p(id(parameter)))
-        func_return = ctypes.cast(func_return_ptr, ctypes.py_object).value # is a `SwiftReturnValue`
+        func_return = ctypes.cast(func_return_ptr, ctypes.py_object).value # `func_return` is a `SwiftReturnValue`
+        if func_return.error is not None: # bridged from `nil` to `Python.None` in PythonKit
+            raise func_return.error
+        return func_return.wrapped_object # `None` if the Swift function doesn't return anything
 #         function_return = self.__bridge_lib(
         
         # call the lib's `callSwiftFromPython` function
@@ -20,5 +22,5 @@ class SwiftObject:
         # otherwise, return the wrapped_object (it might be `None`)
         pass
 class SwiftReturnValue:
-    def __init__(self, wrapped_object, error):
-        self.wrapped_object, self.error = wrapped_object, error # inside the Swift code, this is created from a Swift PythonConvertibleError
+    def __init__(self, wrapped_object, error): # inside the Swift code, `error` is created from a Swift PythonConvertibleError type
+        self.wrapped_object, self.error = wrapped_object, error # `error` conforms to `BaseException`
