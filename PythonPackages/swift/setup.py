670c72c1398d9f4812cfbf40abc1da56c01f1294
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 12:16:30 -0500

    Update setup.py

--- PythonPackages/swift/setup.py
+++ PythonPackages/swift/setup.py
@@ -1,1 +1,3 @@
-from setuptools import setup, find_packages; setup(name="swift", version="5", packages=find_packages())
+from setuptools import setup, find_packages
+
+setup(name="swift", version="5", packages=find_packages())
