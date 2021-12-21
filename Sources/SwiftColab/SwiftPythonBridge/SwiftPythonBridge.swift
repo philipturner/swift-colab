c3016e78125cd138929d8756d8228501291d28bc
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 16:37:37 -0500

    Update SwiftPythonBridge.swift

--- Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
+++ Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
@@ -7,11 +7,7 @@
 // @convention(c) (PythonObject) throws -> PythonObject
 // (PythonObject) throws -> Void
 // (PythonObject) throws -> PythonObject
-// @differentiable(reverse) (Differentiable & ConvertibleFromPython) throws -> Differentiable & PythonConvertible
-// differentiable functions may also have the restrictions of PythonConvertible & ConvertibleFromPython on input and output
-
-// The differentiable function types will be especially hard to implement. I need to find a way to work around not being 
-// able to differentiate the operation of bridging to a Python type. In addition, autodiff isn't fully functional right now.
-// So, differentiable function types won't be supported yet.
 
 // Workaround that lets me avoid modifying PythonKit: make a class that replicates the behavior of PyReference. Then, unsafe bit cast it to a PythonObject.
+
+// could make this a one-file binary
