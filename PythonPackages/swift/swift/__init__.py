a487af2a87f70824526e367ca93fd9078180e768
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 11:41:22 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,9 +1,9 @@
 import subprocess as sp; import ctypes
 def run(swift_string): # will change to directly call a Swift dynamic library via C interop. This should let stdout and stderr be synchronized with the Jupyter kernel.
     p = sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.PIPE, text=True); print(p.stdout + p.stderr)
-class SwiftObject:
-    def __init__(self, function_table):
-        self.function_table = function_table # a `[String: Int]` dictionary of memory addresses of function wrappers, to be initialized in Swift
+class SwiftDelegate:
+    def __init__(self):
+        self.function_table = {} # a `[String: Int]` dictionary of memory addresses of function wrappers, to be initialized in Swift
         self.__bridge_lib = ctypes.CDLL("/opt/swift/lib/libSwiftPythonBridge.so")
         self.__bridge_lib.restype, self.__bridge_lib.argtypes = c_void_p, [c_void_p] # args accessed as `[PythonObject]` in Swift
     def call_swift_func(self, function_name, params): # params should have a `subscript(Int) -> PythonObject` method, will be copied into a `[PythonObject]`
@@ -16,6 +16,8 @@ class SwiftObject:
 class SwiftReturnValue:
     def __init__(self, wrapped_object, error): # `error` is created from a Swift `Error` (may need to make Python classes to help with bridging)
         self.wrapped_object, self.error = wrapped_object, error # `error` conforms to `BaseException`
-class ExampleSwiftObject(SwiftObject): # eventually, will try implementing this in a separate Python package - keeping in here rn for simplicity
+class ExampleSwiftObject(): # eventually, will try implementing this in a separate Python package - keeping in here rn for simplicity
     def __init__(self):
-        super().__init__({})
+        self.swift_delegate = SwiftDelegate()
+    def example_func(self, string_param):
+        swift_delegate.call_swift_func("example_func", [self, string_param])
