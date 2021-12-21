9295c8e3565673252a778eae594e6b663b984d1c
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 12:28:31 -0500

    Update SwiftInteropTest.py

--- PythonPackages/swift/swift/SwiftInteropTest.py
+++ PythonPackages/swift/swift/SwiftInteropTest.py
@@ -1,1 +1,6 @@
-
+class SwiftInteropTest(): 
+    def __init__(self):
+        self.swift_delegate = SwiftDelegate()
+        
+    def example_func(self, string_param):
+        swift_delegate.call_swift_func("example_func", [self, string_param])
