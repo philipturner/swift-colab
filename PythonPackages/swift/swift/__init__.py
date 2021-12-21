70763d3de71af465b19367f31ab164209c61499a
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 10:08:18 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -9,11 +9,15 @@ class SwiftObject:
         self.__bridge_lib.restype = c_void_p; self.__bridge_lib.argtypes = [c_void_p]
         # call a C function on the return value, which optionally returns an error. The return value is a wrapper over the actual returned object
     def call_swift(self, function_name):
-        function_wrapper_address = self.function_table[function_name] # is an integer
+        function_wrapper_address = c_void_p(self.function_table[function_name]) # cast an integer to an `OpaquePointer`
+#         function_return = self.__bridge_lib(
+        
         # call the lib's `callSwiftFromPython` function
         # initialize the `SwiftReturnValue` given the id
+        # if the error object isn't `None`, raise an exception/error
+        # otherwise, return the wrapped_object (it might be `None`)
         pass
-class SwiftReturnValue: # need to initialize this given its id
-    def __init__(self, wrapped_value, error):
-        self.wrappedvalue = wrapped_value
+class SwiftReturnValue:
+    def __init__(self, wrapped_object, error):
+        self.wrapped_object = wrapped_object
         self.error = error # inside the Swift code, this is created from a Swift PythonConvertibleError
