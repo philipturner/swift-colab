9a858a6e58153c3fab2d432a89c5ae4ece7f4cea
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 09:44:19 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -4,5 +4,6 @@ def run(swift_string): # will change to directly call a Swift dynamic library vi
     p = sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.PIPE, text=True); print(p.stdout + p.stderr)
 class SwiftObject:
     def __init__(self, function_table):
-        self.function_table = function_table
-        self.__lib = ctypes.CDLL("/opt/swift/libSwiftPythonBridge.so")
+        self.function_table = function_table # a table of 64-bit pointers to function pointer wrappers
+        self.__bridge_lib = ctypes.CDLL("/opt/swift/libSwiftPythonBridge.so")
+        self.__bridge_lib.restype = c_void_p; self.__bridge_lib.argtypes = [c_void_p] # can also throw an error, but haven't figure out how to do that yet
