a6dfe9413a693cb626defdec2590a8a296b577fe
Philip Turner <philipturner.AR@gmail.com>
2021-12-21 21:33:16 -0500

    Update SwiftPythonBridge.swift

--- Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
+++ Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
@@ -8,10 +8,29 @@
 
 import PythonKit
 
+let swift = Python.import("swift")
+
 @_cdecl("callSwiftFromPython")
 public func callSwiftFromPython(_ functionHandleRef: OpaquePointer, _ params: OwnedPyObjectPointer) -> PyObjectPointer {
     // initialize the function handle ref using Unmanaged<FunctionHandle>
     let paramsObject = PythonObject(params)
+    var returnObject = Python.None
+    var returnError = Python.None
+    
+    // call the function, either get an object or don't
+    
+    try {
+        if ... { /* non-returning closure */
+            _ = ...
+        } else { /* returning closure */
+            returnObject = ...
+        }
+    } catch {
+        returnError = Python.Exception(PythonObject(error.localizedDescription))
+    }
+    
+    
+    
     
     // return the output's borrowedPythonObject
 }
