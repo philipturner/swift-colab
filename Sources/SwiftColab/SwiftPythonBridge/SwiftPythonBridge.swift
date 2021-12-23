150b19c1942b3d109f48ed118ff2eb36d24eacae
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:37:50 -0500

    Delete SwiftPythonBridge.swift

--- Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
+++
@@ -1,6 +0,0 @@
-// Small dynamic library for calling Swift functions from Python.
-// Can also be imported by Swift because it declares the PythonObject
-// methods for managing the function table of a `SwiftDelegate`.
-
-import PythonKit
-let globalSwiftModule = Python.import("swift")
