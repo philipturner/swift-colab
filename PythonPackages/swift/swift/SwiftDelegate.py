1268b7255eb5f411dde9952969f2848b4b1cc2a9
Philip Turner <philipturner.AR@gmail.com>
2021-12-23 16:08:37 -0500

    Update SwiftDelegate.py

--- PythonPackages/swift/swift/SwiftDelegate.py
+++ PythonPackages/swift/swift/SwiftDelegate.py
@@ -1,16 +1,16 @@
-import ctypes
+from ctypes import *
 
 class SwiftDelegate:
     def __init__(self):
         self.function_table = {} # a `[String: Int]` dictionary of memory addresses of function wrappers, to be initialized in Swift
-        self.__call_swift = ctypes.CDLL("/opt/swift/lib/libSwiftPythonBridge.so").callSwiftFromPython
+        self.__call_swift = CDLL("/opt/swift/lib/libSwiftPythonBridge.so").callSwiftFromPython
         self.__call_swift.restype, self.__call_swift.argtypes = c_void_p, [c_void_p, c_void_p] # args accessed as `[PythonObject]` in Swift
     
     # params should have a `subscript(Int) -> PythonObject` method, will be copied into a `[PythonObject]`
     def call_func(self, function_name, params): 
         func_ptr_wrapper_ptr = c_void_p(self.function_table[function_name]) # cast an integer to an `OpaquePointer`
         func_return_ptr = self.__call_swift(func_ptr_wrapper_ptr, c_void_p(id(params)))
-        return ctypes.cast(func_return_ptr, ctypes.py_object).value.unwrap() # `func_return` is a `SwiftReturnValue`
+        return cast(func_return_ptr, py_object).value.unwrap() # `func_return` is a `SwiftReturnValue`
     
 class SwiftError(Exception):
     def __init__(self, localized_description) # created from the Swift Error's `localizedDescription`
