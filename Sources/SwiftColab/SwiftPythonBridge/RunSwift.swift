c5c366dccd790fde107104389cd472cbbee633a9
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:31:09 -0500

    Update RunSwift.swift

--- Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
+++ Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
@@ -1,17 +1,16 @@
-// take a Python string object as input and compile and run it
-
 import Foundation
 import PythonKit
-// pythonSwiftModule is defined in the header file
+fileprivate let swiftModule = globalSwiftModule
 
+// takes a Python string object as input and compile and run it
 @_cdecl("runSwiftAsString")
 public func runSwiftAsString(_ pythonStringRef: OwnedPyObjectPointer) -> PyObjectPointer {
     let pi = ProcessInfo.processInfo
     let path = pi.environment["PATH"]
     
     @inline(never)
-    func getPythonError(stringValue: String) {
-        let 
+    func getPythonError(message: String) -> PythonObject {
+        swiftModule.SwiftError(PythonObject(message))
     }
     
     if !path.starts(with: "/opt/swift/toolchain/usr/bin") {
@@ -19,5 +18,9 @@ public func runSwiftAsString(_ pythonStringRef: OwnedPyObjectPointer) -> PyObjec
     }
     
     let codeString = String(PythonObject(pythonStringRef))
-    let codeData = codeString.data(using: .utf8)
+    guard let codeData = codeString.data(using: .utf8) else {
+        return getPythonError(message: "Python string was not decoded as UTF-8 when compiling a Swift script")
+    }
+    
+    
 }
