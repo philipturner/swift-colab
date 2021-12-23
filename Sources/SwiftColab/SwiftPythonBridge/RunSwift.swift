c13e9d582418750299a8611020479874e0d7cdfd
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:12:49 -0500

    Update RunSwift.swift

--- Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
+++ Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
@@ -1,8 +1,17 @@
 // take a Python string object as input and compile and run it
 
+import Foundation
 import PythonKit
 
 @_cdecl("runSwiftAsString")
 public func runSwiftAsString(_ pythonStringRef: OwnedPyObjectPointer) {
+    let pi = ProcessInfo.processInfo
+    let path = pi.environment["PATH"]
+    
+    if !path.startsWith("/opt/swift/toolchain/usr/bin") {
+        pi.environment["PATH"] = "/opt/swift/toolchain/usr/bin:\(path)"
+    }
+    
+    let codeString = String(PythonObject(pythonStringRef))
     
 }
