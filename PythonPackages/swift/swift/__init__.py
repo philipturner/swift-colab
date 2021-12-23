83fdf8b307af70af584fa5175c85d8b8c65781f1
Philip Turner <philipturner.AR@gmail.com>
2021-12-23 17:37:16 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -7,13 +7,13 @@ def run(swift_string):
     print(p.stdout + p.stderr)
 
 def run_new(swift_string):
-    print("runSwift checkpoint 0")
+    print("runSwift checkpoint 0.1")
     call_compiled_func("/opt/swift/lib/libSwiftPythonBridge.so", "runSwiftAsString", swift_string)
     print("runSwift checkpoint 6")
 
 # the compiled Swift function must import the modified PythonKit
 def call_compiled_func(executable_name, func_name, params):
-    lib = CDLL(executable_name)
+    lib = PyDLL(executable_name)
     func = getattr(lib, func_name)
     func.restype, func.argtypes = c_void_p, [c_void_p]
     func_return_ptr = func(c_void_p(id(params)))
@@ -25,7 +25,7 @@ def call_compiled_func(executable_name, func_name, params):
 class SwiftDelegate:
     def __init__(self):
         self.function_table = {} # a `[String: Int]` dictionary of memory addresses of function wrappers, to be initialized in Swift
-        self.__call_swift = CDLL("/opt/swift/lib/libSwiftPythonBridge.so").callSwiftFromPython
+        self.__call_swift = PyDLL("/opt/swift/lib/libSwiftPythonBridge.so").callSwiftFromPython
         self.__call_swift.restype, self.__call_swift.argtypes = c_void_p, [c_void_p, c_void_p] # args accessed as `[PythonObject]` in Swift
     
     # params should have a `subscript(Int) -> PythonObject` method, will be copied into a `[PythonObject]`
