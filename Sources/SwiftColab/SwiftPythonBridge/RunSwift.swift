ee60b8d00c1c9498f314e7fe1dbea8def4bfeb11
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:07:30 -0500

    Update RunSwift.swift

--- Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
+++ Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
@@ -1,1 +1,8 @@
 // take a Python string object as input and compile and run it
+
+import PythonKit
+
+@_cdecl("runSwiftAsString")
+public func runSwiftAsString(_ pythonStringRef: OwnedPyObjectPointer) {
+    
+}
