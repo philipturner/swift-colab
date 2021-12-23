3f348ffc5ba30de456a57fef736e16d29fcbf76b
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:36:39 -0500

    Update RunSwift.swift

--- Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
+++ Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
@@ -9,18 +9,36 @@ public func runSwiftAsString(_ pythonStringRef: OwnedPyObjectPointer) -> PyObjec
     let path = pi.environment["PATH"]
     
     @inline(never)
-    func getPythonError(message: String) -> PythonObject {
-        swiftModule.SwiftError(PythonObject(message))
+    func getPythonError(message: String) -> PyObjectPointer {
+        swiftModule.SwiftError(PythonObject(message)).borrowedPyObject
     }
     
     if !path.starts(with: "/opt/swift/toolchain/usr/bin") {
         pi.environment["PATH"] = "/opt/swift/toolchain/usr/bin:\(path)"
     }
     
-    let codeString = String(PythonObject(pythonStringRef))
-    guard let codeData = codeString.data(using: .utf8) else {
-        return getPythonError(message: "Python string was not decoded as UTF-8 when compiling a Swift script")
+    let scriptString = String(PythonObject(pythonStringRef))
+    guard let scriptData = scriptString.data(using: .utf8) else {
+        let message = "Python string was not decoded as UTF-8 when compiling a Swift script"
+        print(message)
+        return getPythonError(message: message)
     }
     
+    let targetPath = "/opt/swift/tmp/string_script.swift")
+    FileManager.default.createFile(atPath: targetPath, contents: scriptData)
     
+    let executeScript = Process()
+    executeScript.executableURL = .init(fileURLWithPath: "/usr/bin/env")
+    executeScript.arguments = ["swift", targetPath]
+    executeScript.currentDirectoryURL = .init(fileURLWithPath: "/content")
+    
+    do {
+        try executeScript.run()
+    } catch {
+        print(error.localizedDescription)
+        return getPythonError(message: error.localizedDescription)
+    }
+    
+    executeScript.waitUntilExit()
+    return Python.None.borrowedPythonObject
 }
