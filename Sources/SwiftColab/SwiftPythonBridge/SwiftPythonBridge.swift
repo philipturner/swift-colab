541b10a3a2d2b2b5c0583672fe94d40c5d1e3b28
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:20:23 -0500

    Update SwiftPythonBridge.swift

--- Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
+++ Sources/SwiftColab/SwiftPythonBridge/SwiftPythonBridge.swift
@@ -2,29 +2,5 @@
 // Can also be imported by Swift because it declares the PythonObject
 // methods for managing the function table of a `SwiftDelegate`.
 
-// Accepts two Swift types:
-// (PythonObject) throws -> Void
-// (PythonObject) throws -> PythonObject
-
 import PythonKit
-let pythonSwiftModule = Python.import("swift")
-
-@_cdecl("callSwiftFromPython")
-public func callSwiftFromPython(_ functionHandleRef: UnsafeRawPointer, _ params: OwnedPyObjectPointer) -> PyObjectPointer {
-    let functionHandle = Unmanaged<FunctionHandle>.fromOpaque(functionHandleRef).takeUnretainedValue()
-    let params = PythonObject(params)
-    
-    var wrappedObject: PythonObject
-    var errorObject: PythonObject
-    
-    do {
-        wrappedObject = try functionHandle.call(params)
-        errorObject = Python.None
-    } catch {
-        wrappedObject = Python.None
-        errorObject = pythonSwiftModule.SwiftError(PythonObject(error.localizedDescription))
-    }
-    
-    let returnValue = pythonSwiftModule.SwiftReturnValue(wrappedObject, errorObject)
-    return returnValue.borrowedPyObject
-}
+let globalSwiftModule = Python.import("swift")
