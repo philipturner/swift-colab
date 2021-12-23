f2183772e3e562f7ec6256ccdd739f4156bd86ad
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:19:53 -0500

    Update CallSwift.swift

--- Sources/SwiftColab/SwiftPythonBridge/CallSwift.swift
+++ Sources/SwiftColab/SwiftPythonBridge/CallSwift.swift
@@ -1,1 +1,22 @@
+import PythonKit
+fileprivate let swiftModule = pythonSwiftModule
 
+@_cdecl("callSwiftFromPython")
+public func callSwiftFromPython(_ functionHandleRef: UnsafeRawPointer, _ params: OwnedPyObjectPointer) -> PyObjectPointer {
+    let functionHandle = Unmanaged<FunctionHandle>.fromOpaque(functionHandleRef).takeUnretainedValue()
+    let params = PythonObject(params)
+    
+    var wrappedObject: PythonObject
+    var errorObject: PythonObject
+    
+    do {
+        wrappedObject = try functionHandle.call(params)
+        errorObject = Python.None
+    } catch {
+        wrappedObject = Python.None
+        errorObject = swiftModule.SwiftError(PythonObject(error.localizedDescription))
+    }
+    
+    let returnValue = swiftModule.SwiftReturnValue(wrappedObject, errorObject)
+    return returnValue.borrowedPyObject
+}
