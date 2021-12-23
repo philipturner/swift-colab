347bbcd8934a0a348ace54aca980852a03789f5a
Philip Turner <philipturner.AR@gmail.com>
2021-12-22 20:17:42 -0500

    Update RunSwift.swift

--- Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
+++ Sources/SwiftColab/SwiftPythonBridge/RunSwift.swift
@@ -2,16 +2,22 @@
 
 import Foundation
 import PythonKit
+// pythonSwiftModule is defined in the header file
 
 @_cdecl("runSwiftAsString")
-public func runSwiftAsString(_ pythonStringRef: OwnedPyObjectPointer) {
+public func runSwiftAsString(_ pythonStringRef: OwnedPyObjectPointer) -> PyObjectPointer {
     let pi = ProcessInfo.processInfo
     let path = pi.environment["PATH"]
+    
+    @inline(never)
+    func getPythonError(stringValue: String) {
+        let 
+    }
     
     if !path.starts(with: "/opt/swift/toolchain/usr/bin") {
         pi.environment["PATH"] = "/opt/swift/toolchain/usr/bin:\(path)"
     }
     
     let codeString = String(PythonObject(pythonStringRef))
-    
+    let codeData = codeString.data(using: .utf8)
 }
