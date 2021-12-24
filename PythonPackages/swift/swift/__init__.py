83b81a6a9729703cc461c288a73934f41ca21da8
Philip Turner <philipturner.AR@gmail.com>
2021-12-24 13:27:55 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,16 +1,16 @@
 from ctypes import *
-from wurlitzer import sys_pipes, sys_pipes_forever
+from wurlitzer import sys_pipes
 
 def run(swift_string):
-    call_compiled_func("/opt/swift/lib/libSwiftPythonBridge.so", "runSwiftAsString", swift_string)
+    with sys_pipes():
+        call_compiled_func("/opt/swift/lib/libSwiftPythonBridge.so", "runSwiftAsString", swift_string)
 
 # the compiled Swift function must import the modified PythonKit
 def call_compiled_func(executable_name, func_name, params):
     lib = PyDLL(executable_name)
     func = getattr(lib, func_name)
     func.restype, func.argtypes = c_void_p, [c_void_p]
-    with sys_pipes_forever(): # this should crash
-        func_return_ptr = func(c_void_p(id(params)))
+    func_return_ptr = func(c_void_p(id(params)))
     output = cast(func_return_ptr, py_object).value.unwrap() # `func_return` is a `SwiftReturnValue`
     return output
 
@@ -22,34 +22,9 @@ class SwiftDelegate:
     
     def call(self, function_name, params): 
         func_ptr_wrapper_ptr = c_void_p(self.function_table[function_name]) # cast an integer to an `OpaquePointer`
-        with sys_pipes():
-            func_return_ptr = self.__call_swift(func_ptr_wrapper_ptr, c_void_p(id(params)))
+        func_return_ptr = self.__call_swift(func_ptr_wrapper_ptr, c_void_p(id(params)))
         return cast(func_return_ptr, py_object).value.unwrap() # `func_return` is a `SwiftReturnValue`
-    
-    def call_2(self, function_name, params):
-        self.call(function_name, params)
-        
-    def call_3(self, function_name, params):
-        print("call_3 intro")
-        self.call(function_name, params)
-        print("call_3 outtro")
-        
-    def call_alt(self, function_name, params): 
-        func_ptr_wrapper_ptr = c_void_p(self.function_table[function_name]) # cast an integer to an `OpaquePointer`
-        with sys_pipes():
-            func_return_ptr = self.__call_swift(func_ptr_wrapper_ptr, c_void_p(id(params)))
-        return cast(func_return_ptr, py_object).value.unwrap() # `func_return` is a `SwiftReturnValue`
-    
-    def call_2_alt(self, function_name, params):
-        with sys_pipes():
-            self.call_alt(function_name, params)
-        
-    def call_3_alt(self, function_name, params):
-        print("call_3_alt intro")
-        with sys_pipes():
-            self.call_alt(function_name, params)
-        print("call_3_alt outtro")
-    
+
 class SwiftError(Exception):
     def __init__(self, localized_description):
         super().__init__(localized_description)
@@ -64,7 +39,7 @@ class SwiftReturnValue:
             raise self.__error
         return self.__wrapped
 
-class SwiftInteropTest(): 
+class SwiftInteropTest: 
     def __init__(self):
         self.swift_delegate = SwiftDelegate()
         
