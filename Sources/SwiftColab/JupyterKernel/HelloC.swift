5a561b465d07863b4db79a468ca7736a2e984e8e
Philip Turner <philipturner.AR@gmail.com>
2021-12-24 18:06:29 -0500

    Update HelloC.swift

--- Sources/SwiftColab/JupyterKernel/HelloC.swift
+++ Sources/SwiftColab/JupyterKernel/HelloC.swift
@@ -5,23 +5,23 @@ let swiftModule = Python.import("swift")
 
 @_cdecl("helloC")
 public func helloC(_ meaningOfLife: Int32) -> Int32 {
-    print("meaning of life: \\\(meaningOfLife)")
+    print("meaning of life: \(meaningOfLife)")
 
     let interopTest = swiftModule.SwiftInteropTest()
 
     // Direct the logic of SwiftInteropTest's methods from Python to Swift
 
     interopTest.registerFunction(name: "example_func") { param -> Void in
-        print("example_func called from Python with param \\\(param)")
+        print("example_func called from Python with param \(param)")
     }
 
     interopTest.registerFunction(name: "example_func_2") { param -> PythonConvertible in
-        print("example_func_2 called from Python with param \\\(param)")
+        print("example_func_2 called from Python with param \(param)")
         return String("return value")
     }
 
     interopTest.registerFunction(name: "example_func_3") { param -> PythonObject in
-        print("example_func_3 called from Python with param \\\(param)")
+        print("example_func_3 called from Python with param \(param)")
         return PythonObject("return value")
     }
 
