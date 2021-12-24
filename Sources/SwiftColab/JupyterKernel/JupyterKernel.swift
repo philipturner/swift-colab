e989772fb6128267836be30cb603711222b28d1d
Philip Turner <philipturner.AR@gmail.com>
2021-12-24 18:49:32 -0500

    Update JupyterKernel.swift

--- Sources/SwiftColab/JupyterKernel/JupyterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/JupyterKernel.swift
@@ -3,3 +3,18 @@ import PythonKit
 import SwiftPythonBridge
 // import the Python Jupyter kernel library too
 fileprivate let swiftModule = Python.import("swift")
+
+@_cdecl("JupyterKernelCreate")
+public func JupyterKernelCreate(_ jupyterKernelRef: OwnedPyObjectPointer) -> PyObjectPointer {
+    let noneObject = Python.None
+    var errorObject = noneObject
+    
+    do {
+        let jupyterKernel = PythonObject(jupyterKernelRef)
+    } catch {
+        errorObject = swiftModule.SwiftError(error.localizedDescription)
+    }
+
+    
+    return swiftModule.SwiftReturnValue(noneObject, errorObject).ownedPyObject
+}
