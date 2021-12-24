2f7f9ab401868a3ccca0a4dda0593ec0cf23418e
Philip Turner <philipturner.AR@gmail.com>
2021-12-24 14:21:45 -0500

    Update FunctionHandle.swift

--- Sources/SwiftColab/SwiftPythonBridge/FunctionHandle.swift
+++ Sources/SwiftColab/SwiftPythonBridge/FunctionHandle.swift
@@ -31,7 +31,7 @@ extension PythonObject {
         
         let function_table = self.swift_delegate.function_table
         
-        if let retrievedObject = function_table[name], let previousAddress = Int(retrievedObject) {
+        if let previousAddress = Int(function_table.checking[name]) {
            releaseFunction(address: previousAddress)
         }
         
@@ -43,7 +43,7 @@ extension PythonObject {
     public func releaseFunction(name: String) throws {
         let function_table = self.swift_delegate.function_table
         
-        guard let address = Int(function_table[name]) else {
+        guard let address = Int(function_table.checking[name]) else {
             struct ReleaseFunctionError: Error { let localizedDescription: String }
             throw ReleaseFunctionError(localizedDescription: "Attempted to release a non-retained \(name) function on a Python object")
         }
