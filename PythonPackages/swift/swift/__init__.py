ed45899da2ae336b35b4baed987bf7bd31bd45f3
Philip Turner <philipturner.AR@gmail.com>
2021-12-23 20:19:31 -0500

    Update __init__.py

--- PythonPackages/swift/swift/__init__.py
+++ PythonPackages/swift/swift/__init__.py
@@ -1,25 +1,24 @@
 import subprocess as sp
 from ctypes import *
+from wurlitzer import sys_pipes
 
+# remove the legacy run command very soon, and the subprocess import with it
 # calling run_swift using thorugh a pre-compiled function instead should let stdout and stderr be synchronized with the Jupyter kernel.
 def run(swift_string):
     p = sp.run(["bash", "/opt/swift/run_swift.sh", swift_string], stdout=sp.PIPE, stderr=sp.PIPE, text=True)
     print(p.stdout + p.stderr)
 
 def run_new(swift_string):
-    print("runSwift checkpoint 0.1")
     call_compiled_func("/opt/swift/lib/libSwiftPythonBridge.so", "runSwiftAsString", swift_string)
-    print("runSwift checkpoint 6")
 
 # the compiled Swift function must import the modified PythonKit
 def call_compiled_func(executable_name, func_name, params):
     lib = PyDLL(executable_name)
     func = getattr(lib, func_name)
     func.restype, func.argtypes = c_void_p, [c_void_p]
-    func_return_ptr = func(c_void_p(id(params)))
-    print("runSwift checkpoint 4")
+    with sys_pipes():
+        func_return_ptr = func(c_void_p(id(params)))
     output = cast(func_return_ptr, py_object).value.unwrap() # `func_return` is a `SwiftReturnValue`
-    print("runSwift checkpoint 5")
     return output
 
 class SwiftDelegate:
@@ -31,7 +30,8 @@ class SwiftDelegate:
     # params should have a `subscript(Int) -> PythonObject` method, will be copied into a `[PythonObject]`
     def call_func(self, function_name, params): 
         func_ptr_wrapper_ptr = c_void_p(self.function_table[function_name]) # cast an integer to an `OpaquePointer`
-        func_return_ptr = self.__call_swift(func_ptr_wrapper_ptr, c_void_p(id(params)))
+        with sys_pipes:
+            func_return_ptr = self.__call_swift(func_ptr_wrapper_ptr, c_void_p(id(params)))
         return cast(func_return_ptr, py_object).value.unwrap() # `func_return` is a `SwiftReturnValue`
     
 class SwiftError(Exception):
