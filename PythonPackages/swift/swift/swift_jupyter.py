e9779d802b0e38b296d6d18adf29b3b1493a9d45
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 09:34:20 -0500

    Create swift_jupyter.py

---
+++ PythonPackages/swift/swift/swift_jupyter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# this file must have chmod set to a+x
+import ctypes
+import swift
+
+# define a subclass of jupyter's Kernel class
+# define any other absolutely necessary subclasses
+
+# see if I can import stuff declared in __init__.py
+
+if __name__ == "__main__":
+    # register the kernel in IPKernelApp
+    print("called main")
+else:
+    # should never be called
+    print("did not call main")
