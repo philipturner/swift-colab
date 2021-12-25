4e4261eb4a1ca57b00e72765e4e0e8e4180687f2
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 09:41:42 -0500

    Delete PythonPackages/swift_jupyter directory

--- PythonPackages/swift_jupyter/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_packages
-# no sense in pip installing this because it doesn't define any symbols the Swift code needs to know beforehand
-setup(name="swift_jupyter", version="5", packages=find_packages())
