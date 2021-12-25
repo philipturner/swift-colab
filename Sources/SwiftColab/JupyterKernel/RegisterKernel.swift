ea18c1b38bb25c651a0d1d1f506314a679e8ca46
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 11:06:27 -0500

    Update RegisterKernel.swift

--- Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
@@ -1,7 +1,10 @@
 import PythonKit
 import SwiftPythonBridge
 import Foundation
+
 fileprivate let swiftModule = Python.import("swift")
+fileprivate let json = Python.import("json")
+fileprivate let os = Python.import("os")
 
 @_cdecl("JKRegisterKernel")
 public func JKRegisterKernel() -> Void {
