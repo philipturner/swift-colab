c0d049ff1eeef5096972b57876182eb1e46b32c2
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 10:09:04 -0500

    Update Register.swift

--- Sources/SwiftColab/JupyterKernel/Register.swift
+++ Sources/SwiftColab/JupyterKernel/Register.swift
@@ -1,1 +1,34 @@
 // to be compiled into the dylib and called by the install_swift script, synchronizing its output with shell (need to ensure that's the case)
+
+import PythonKit
+import SwiftPythonBridge
+import Foundation
+fileprivate let swiftModule = Python.import("swift")
+
+@_cdecl("JKRegisterKernel")
+public func helloC(_ meaningOfLife: Int32) -> Int32 {
+    print("meaning of life: \(meaningOfLife)")
+
+    let interopTest = swiftModule.SwiftInteropTest()
+
+    // Direct the logic of SwiftInteropTest's methods from Python to Swift
+    interopTest.registerFunction(name: "example_func") { param -> Void in
+        print("example_func called from Python with param \(param)")
+    }
+
+    interopTest.registerFunction(name: "example_func_2") { param -> PythonConvertible in
+        print("example_func_2 called from Python with param \(param)")
+        return String("return value")
+    }
+
+    interopTest.registerFunction(name: "example_func_3") { param -> PythonObject in
+        print("example_func_3 called from Python with param \(param)")
+        return PythonObject("return value")
+    }
+
+    print(interopTest.example_func("Input string for example_func"))
+    print(interopTest.example_func_2("Input string for example_func_2"))
+    print(interopTest.example_func_3("Input string for example_func_3"))
+
+    return meaningOfLife + 100
+}
