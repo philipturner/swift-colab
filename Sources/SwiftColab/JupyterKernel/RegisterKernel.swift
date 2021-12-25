e7d7c829c4151438d75937e2c62ce228f3e086a3
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 11:01:55 -0500

    Update RegisterKernel.swift

--- Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
@@ -1,5 +1,3 @@
-// to be compiled into the dylib and called by the install_swift script, synchronizing its output with shell (need to ensure that's the case)
-
 import PythonKit
 import SwiftPythonBridge
 import Foundation
@@ -7,6 +5,7 @@ fileprivate let swiftModule = Python.import("swift")
 
 @_cdecl("JKRegisterKernel")
 public func JKRegisterKernel() -> Void {
-    print(swiftModule.SwiftReturnValue)
-    print(52)
+    print()
+    print("=== Registering Swift Jupyter kernel ===")
+    defer { print("=== Finished registering Swift Jupyter kernel ===") }
 }
