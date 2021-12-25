50b51208098a585a003f5d22dc5de5ad5e3ae1a1
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 13:18:21 -0500

    Update RegisterKernel.swift

--- Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
@@ -46,8 +46,7 @@ public func JKRegisterKernel() -> Void {
             f.close()
         }
         
-        // TODO: Ensure this doesn't throw an OSError because the process doesn't have appropriate permissions
-//         KernelSpecManager().install_kernel_spec(td.name, kernel_code_name)
+        KernelSpecManager().install_kernel_spec(td.name, kernel_code_name)
         td.cleanup()
     }
     
