0387f2030381f5dd535b24b76532a085c0c6992e
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 09:44:31 -0500

    Create swift_kernel.py

---
+++ Sources/SwiftColab/JupyterKernel/swift_kernel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# this file must have chmod set to a+x
+import swift
+from ctypes import *
+import wurlitzer
+
+# symbols for the Jupyter kernel
+
+# ...
+
+# define a subclass of jupyter's Kernel class
+# define any other absolutely necessary subclasses
+
+if __name__ == "__main__":
+    # register the kernel in IPKernelApp
+    # may need to use wurlitzer.sys_pipes - must validate that the called Swift code can log to output
+    print("called main")
+    print(SwiftDelegate)
+else:
+    # should never be called
+    print("did not call main")
