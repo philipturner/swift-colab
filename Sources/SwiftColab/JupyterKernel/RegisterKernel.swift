9bc4ad83d13b56fc2fb49fc79d7d426a123021f0
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 11:04:39 -0500

    Update RegisterKernel.swift

--- Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
@@ -8,4 +8,7 @@ public func JKRegisterKernel() -> Void {
     print()
     print("=== Registering Swift Jupyter kernel ===")
     defer { print("=== Finished registering Swift Jupyter kernel ===") }
+    
+    let kernel_name: PythonObject = "Swift"
+    let kernel_code_name: PythonObject = "swift"
 }
