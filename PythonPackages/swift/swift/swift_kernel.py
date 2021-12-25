1c63c975d4d343ff7d0a5a98c2cf1e0733d5372e
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 09:10:00 -0500

    Update swift_kernel.py

--- PythonPackages/swift/swift/swift_kernel.py
+++ PythonPackages/swift/swift/swift_kernel.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python
+# this file must have chmod set to a+x
 
+# define a subclass of jupyter's Kernel class
+# define any other absolutely necessary subclasses
+
+if __name__ == "__main__":
+    # register the kernel in IPKernelApp
+    print("called main")
+else:
+    # should never be called
+    print("did not call main")
+""")
