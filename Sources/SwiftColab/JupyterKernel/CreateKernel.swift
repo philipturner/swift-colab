a85e7e04d0041f0f055e0a1e5ac205f346331f0f
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 10:13:23 -0500

    Update CreateKernel.swift

--- Sources/SwiftColab/JupyterKernel/CreateKernel.swift
+++ Sources/SwiftColab/JupyterKernel/CreateKernel.swift
@@ -8,19 +8,13 @@ fileprivate let swiftModule = Python.import("swift")
 public func JKCreateKernel(_ jupyterKernelRef: OwnedPyObjectPointer) -> OwnedPyObjectPointer {
     let noneObject = Python.None
     let errorObject = noneObject
-//     var errorObject = noneObject
     
-//     do {
     let jupyterKernel = PythonObject(jupyterKernelRef)
     jupyterKernel.swift_delegate = swiftModule.SwiftDelegate()
     
     jupyterKernel.registerFunction(name: "helloC") { params -> PythonConvertible in
         helloC(Int32(params)!)
     }
-//     } catch {
-//         print(error.localizedDescription)
-//         errorObject = swiftModule.SwiftError(error.localizedDescription)
-//     }
     
     return swiftModule.SwiftReturnValue(noneObject, errorObject).ownedPyObject
 }
