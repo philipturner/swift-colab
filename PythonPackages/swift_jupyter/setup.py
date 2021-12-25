34d9e01d5de324d7b780af282b7fef6295ce55c1
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 09:12:44 -0500

    Update setup.py

--- PythonPackages/swift_jupyter/setup.py
+++ PythonPackages/swift_jupyter/setup.py
@@ -1,1 +1,3 @@
-
+from setuptools import setup, find_packages
+# no sense in pip installing this because it doesn't define any symbols the Swift code needs to know beforehand
+setup(name="swift_jupyter", version="5", packages=find_packages())
