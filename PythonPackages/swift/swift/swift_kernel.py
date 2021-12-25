547f86d380e569dbbfe2f03c6c9e4aa5eba26f65
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 12:47:54 -0500

    Update swift_kernel.py

--- PythonPackages/swift/swift/swift_kernel.py
+++ PythonPackages/swift/swift/swift_kernel.py
@@ -15,15 +15,20 @@ from ctypes import *
 SwiftError = swift.SwiftError
 
 if __name__ == "__main__":
-    signal.pthread_sigmask(signal.SIG_BLOCK, [signal.SIGINT])
+    print("called main 0")
+    
+    with sys_pipes():
+        print("called main")
+#     signal.pthread_sigmask(signal.SIG_BLOCK, [signal.SIGINT])
     
     # register the kernel in IPKernelApp
     # may need to use wurlitzer.sys_pipes - must validate that the called Swift code can log to output
-    with sys_pipes(): # if logging doesn't work, try writing to a file
-        print("called main")
-        print(swift.SwiftDelegate)
-        print(SwiftError)
+#     with sys_pipes(): # if logging doesn't work, try writing to a file
+#         print("called main")
+#         print(swift.SwiftDelegate)
+#         print(SwiftError)
 else:
     # should never be called
+    print("did not call main 0")
     with sys_pipes():
         print("did not call main")
