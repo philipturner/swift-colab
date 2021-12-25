9b2286996c428bbca1f8e1068e1f5bc5f7a8375a
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 11:46:42 -0500

    Update RegisterKernel.swift

--- Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
@@ -66,9 +66,9 @@ fileprivate func validate_kernel_env(_ kernel_env: PythonObject) throws {
         throw Exception("python library not found at \(filePath)")
     }
     
-    let lib_paths = kernel_env["LD_LIBRARY_PATH"].split(":")
+    let lib_paths = kernel_env["LD_LIBRARY_PATH"].split(separator: ":")
     
-    for tupleObject in PythonObject.enumerate(lib_paths) {
+    for tupleObject in Python.enumerate(lib_paths) {
         
     }
 }
