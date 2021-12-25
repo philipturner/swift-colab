49bcf1918881a14b8162fe67ab1515a098227a84
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 09:38:16 -0500

    Update swift_jupyter.py

--- PythonPackages/swift/swift/swift_jupyter.py
+++ PythonPackages/swift/swift/swift_jupyter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # this file must have chmod set to a+x
 import ctypes
+from __init__ import *
 
 # define a subclass of jupyter's Kernel class
 # define any other absolutely necessary subclasses
