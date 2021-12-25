eb51aced8f3587cdbaa847abb7ce1630d7f0d49d
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 11:24:59 -0500

    Update RegisterKernel.swift

--- Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
@@ -15,6 +15,8 @@ public func JKRegisterKernel() -> Void {
     print("=== Registering Swift Jupyter kernel ===")
     defer { print("=== Finished registering Swift Jupyter kernel ===") }
     
+    let kernel_env = make_kernel_env()
+    
     let kernel_name: PythonObject = "Swift"
     let kernel_code_name: PythonObject = "swift"
     
@@ -26,7 +28,10 @@ func make_kernel_env() -> PythonObject {
     
     let kernel_env: PythonObject = [:]
     kernel_env["PYTHONPATH"] = "\(swift_toolchain)/usr/lib/python3/dist-packages"
-    
+    kernel_env["LD_LIBRARY_PATH"] = "\(swift_toolchain)/usr/lib/swift/linux"
+    kernel_env["REPL_SWIFT_PATH"] = "\(swift_toolchain)/usr/bin/repl_swift"
+    kernel_env["SWIFT_BUILD_PATH"] = "\(swift_toolchain)/usr/bin/swift-build"
+    kernel_env["SWIFT_PACKAGE_PATH"] = "\(swift_toolchain)/usr/bin/swift-package"
     
     return kernel_env
 }
