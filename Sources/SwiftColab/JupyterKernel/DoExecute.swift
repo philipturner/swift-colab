7e5ab573e2c2cd7c1dea67df9d4ee48a7b12a0cb
Philip Turner <philipturner.AR@gmail.com>
2021-12-26 17:12:26 -0500

    Update DoExecute.swift

--- Sources/SwiftColab/JupyterKernel/DoExecute.swift
+++ Sources/SwiftColab/JupyterKernel/DoExecute.swift
@@ -2,6 +2,12 @@ import Foundation
 import PythonKit
 import SwiftPythonBridge
 
-internal func doExecute(_ kwargs: PythonObject) -> PythonObject {
+internal func doExecute(_ kwargs: PythonObject) throws -> PythonObject {
+    struct Exception: Error {
+        let localizedDescription = "Intentionally causing a crash from Swift'
+    }
+    
+    throw Exception()
+    
     return Python.None
 }
