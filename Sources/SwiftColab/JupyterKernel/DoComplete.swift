009f215209e9a360af909e8a32afa8564848cc87
Philip Turner <philipturner.AR@gmail.com>
2021-12-26 18:49:29 -0500

    Update DoComplete.swift

--- Sources/SwiftColab/JupyterKernel/DoComplete.swift
+++ Sources/SwiftColab/JupyterKernel/DoComplete.swift
@@ -1,3 +1,24 @@
 import Foundation
 import PythonKit
 import SwiftPythonBridge
+
+func doComplete(_ kwargs: PythonObject) throws -> PythonObject {
+    let selfRef = kwargs["self"]
+    
+    struct CompletionNotImplementedError: LocalizedError {
+        let errorDescription: String? = "`do_complete` has not been implemented for the Swift Kernel."
+    }
+    
+    throw CompletionNotImplementedError() // intentionally crashing before, not after the if statement to ensure it works
+    
+    if !Bool(selfRef.completion_enabled)! {
+        let cursor_pos = kwargs["cursor_pos"]
+        
+        return [
+            "status": "ok",
+            "matches": [],
+            "cursor_start": cursor_pos,
+            "cursor_end": cursor_pos,
+        ]
+    }
+}
