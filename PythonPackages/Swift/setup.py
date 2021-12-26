1f9cc33f1e7d06ba98c4c9274ed19d2a3e625638
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 20:12:52 -0500

    Update and rename PythonPackages/swift/setup.py to PythonPackages/Swift/setup.py

---
+++ PythonPackages/Swift/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="Swift", version="5", packages=find_packages())
--- PythonPackages/swift/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="swift", version="5", packages=find_packages())
