fd942f71d41ad4bc2074740e713aac76ada1df38
Philip Turner <philipturner.AR@gmail.com>
2021-12-26 18:01:56 -0500

    Update DoExecute.swift

--- Sources/SwiftColab/JupyterKernel/DoExecute.swift
+++ Sources/SwiftColab/JupyterKernel/DoExecute.swift
@@ -3,11 +3,14 @@ import PythonKit
 import SwiftPythonBridge
 
 internal func doExecute(_ kwargs: PythonObject) throws -> PythonObject {
-    struct Exception: LocalizedError {
-        let errorDescription: String? = "Intentionally causing a crash from Swift"
-    }
+    let selfRef = kwargs["self"]
     
-    throw Exception()
+    if !Bool(kwargs["silent"])! {
+        let stream_content: PythonObject = ["name": "stdout", "text": kwargs["code"]]
+        
+        let throwingObject = selfRef.send_response.throwing
+        try throwingObject.dynamicallyCall(withArguments: selfRef.iopub_socket, "stream", stream_content)
+    }
     
     return Python.None
 }
