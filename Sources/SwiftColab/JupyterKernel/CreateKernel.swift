cf5cd214fe273e7c849a117e6a9c4d035d16798a
Philip Turner <philipturner.AR@gmail.com>
2021-12-25 20:12:07 -0500

    Update CreateKernel.swift

--- Sources/SwiftColab/JupyterKernel/CreateKernel.swift
+++ Sources/SwiftColab/JupyterKernel/CreateKernel.swift
@@ -1,8 +1,7 @@
 import Foundation
 import PythonKit
 import SwiftPythonBridge
-// import the Python Jupyter kernel library too
-fileprivate let swiftModule = Python.import("swift")
+fileprivate let SwiftModule = Python.import("Swift")
 
 @_cdecl("JKCreateKernel")
 public func JKCreateKernel(_ jupyterKernelRef: OwnedPyObjectPointer) -> OwnedPyObjectPointer {
@@ -10,11 +9,11 @@ public func JKCreateKernel(_ jupyterKernelRef: OwnedPyObjectPointer) -> OwnedPyO
     let errorObject = noneObject
     
     let jupyterKernel = PythonObject(jupyterKernelRef)
-    jupyterKernel.swift_delegate = swiftModule.SwiftDelegate()
+    jupyterKernel.swift_delegate = SwiftModule.SwiftDelegate()
     
     jupyterKernel.registerFunction(name: "helloC") { params -> PythonConvertible in
         helloC(Int32(params)!)
     }
     
-    return swiftModule.SwiftReturnValue(noneObject, errorObject).ownedPyObject
+    return SwiftModule.SwiftReturnValue(noneObject, errorObject).ownedPyObject
 }
