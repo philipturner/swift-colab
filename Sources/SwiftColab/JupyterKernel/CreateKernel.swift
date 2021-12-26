9fff2a84ab1f68459853a57e7e50feb77917f1ab
Philip Turner <philipturner.AR@gmail.com>
2021-12-26 16:17:39 -0500

    Update CreateKernel.swift

--- Sources/SwiftColab/JupyterKernel/CreateKernel.swift
+++ Sources/SwiftColab/JupyterKernel/CreateKernel.swift
@@ -8,11 +8,34 @@ public func JKCreateKernel(_ jupyterKernelRef: OwnedPyObjectPointer) -> OwnedPyO
     let noneObject = Python.None
     let errorObject = noneObject
     
-    let jupyterKernel = PythonObject(jupyterKernelRef)
-    jupyterKernel.swift_delegate = SwiftModule.SwiftDelegate()
+    ///
+    print("Starting initialization of Swift Kernel from Swift")
+    defer { print("Finishing initialization of Swift Kernel from Swift") }
+    ///
     
-    jupyterKernel.registerFunction(name: "helloC") { params -> PythonConvertible in
-        helloC(Int32(params)!)
+    let kernel = PythonObject(jupyterKernelRef)
+    kernel.implementation = "SwiftKernel"
+    kernel.implementation_version = "0.1"
+    kernel.banner = ""
+    
+    kernel.language_info = [
+        "name": "swift",
+        "mimetype": "text/x-swift",
+        "file_extension": ".swift",
+        "version": ""
+    ]
+    
+    // We don't initialize Swift yet, so that the user has a chance to
+    // "%install" packages before Swift starts. (See doc comment in
+    // `_init_swift`).
+    
+    // Whether to do code completion. Since the debugger is not yet
+    // initialized, we can't do code completion yet.
+    kernel.completion_enabled = false
+    kernel.swift_delegate = SwiftModule.SwiftDelegate()
+    
+    kernel.registerFunction(name: "do_execute") { params -> PythonConvertible in
+        PythonObject(2)
     }
     
     return SwiftModule.SwiftReturnValue(noneObject, errorObject).ownedPyObject
