2edad3eaa4300845eab35d7e1c314fb668dd66c2
Philip Turner <philipturner.AR@gmail.com>
2021-12-26 21:18:47 -0500

    Update RunStdoutHandler.swift

--- Sources/SwiftColab/JupyterKernel/StdoutHandler/RunStdoutHandler.swift
+++ Sources/SwiftColab/JupyterKernel/StdoutHandler/RunStdoutHandler.swift
@@ -29,14 +29,14 @@ fileprivate func sendStdout(kernel: PythonObject, stdout: PythonObject) throws {
     
     if clear_sequence_index != -1 {
         try sendStdout(kernel: kernel, stdout:
-                       stdout[((...clear_sequence_index) as PartialRangeUpTo<PythonConvertible>).pythonObject])
+                       stdout[((...clear_sequence_index) as PartialRangeUpTo<PythonConvertible & Comparable>).pythonObject])
         
         try kernel.send_response.throwing.dynamicallyCall(withArguments:
             kernel.iopub_socket, "clear_output", ["wait": false])
             
 
         try sendStdout(kernel: kernel, stdout:
-                       stdout[(((clear_sequence_index + clear_sequence_length)...) as PartialRangeFrom<PythonConvertible>).pythonObject])
+                       stdout[(((clear_sequence_index + clear_sequence_length)...) as PartialRangeFrom<PythonConvertible & Comparable>).pythonObject])
     } else {
         try kernel.send_response.throwing.dynamicallyCall(withArguments:
             kernel.iopub_socket, "stream", ["name": "stdout", "text": stdout])
