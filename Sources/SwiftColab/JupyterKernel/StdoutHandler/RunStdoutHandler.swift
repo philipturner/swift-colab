715259182321a6e946ce6140ab649179be45cb2f
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 15:59:02 -0500

    Update RunStdoutHandler.swift

--- Sources/SwiftColab/JupyterKernel/StdoutHandler/RunStdoutHandler.swift
+++ Sources/SwiftColab/JupyterKernel/StdoutHandler/RunStdoutHandler.swift
@@ -1,7 +1,25 @@
 import Foundation
 import PythonKit
 
-fileprivate func getStdout(_ selfRef: PythonObject) throws -> [PythonObject] {
+func runStdoutHandler(_ selfRef: PythonObject) -> PythonObject {
+    do {
+        while true {
+            if Bool(selfRef.stop_event.wait(0.1))! {
+                break
+            }
+            
+            try get_and_send_stdout(selfRef)
+        }
+        
+        try get_and_send_stdout(selfRef)
+    } catch(let e) {
+        selfRef.kernel.log.error("Exception in StdoutHandler: \(Python.str(e.localizedDescription))")
+    }
+    
+    return Python.None
+}
+
+fileprivate func get_stdout(_ selfRef: PythonObject) throws -> [PythonObject] {
     var output: [PythonObject] = []
     
     while true {
@@ -22,50 +40,32 @@ fileprivate func getStdout(_ selfRef: PythonObject) throws -> [PythonObject] {
 // Sends stdout to the jupyter client, replacing the ANSI sequence for
 // clearing the whole display with a 'clear_output' message to the jupyter
 // client.
-fileprivate func sendStdout(kernel: PythonObject, stdout: PythonObject) throws {
+fileprivate func send_stdout(kernel: PythonObject, stdout: PythonObject) throws {
     let clear_sequence: PythonObject = "\033[2J"
     let clear_sequence_index = stdout.find(clear_sequence)
     let clear_sequence_length = Python.len(clear_sequence)
     
     if clear_sequence_index != -1 {
-        try sendStdout(kernel: kernel, stdout:
-                       stdout[(..<clear_sequence_index).pythonObject])
+        try send_stdout(kernel: kernel, stdout:
+                        stdout[(..<clear_sequence_index).pythonObject])
         
         try kernel.send_response.throwing.dynamicallyCall(withArguments:
             kernel.iopub_socket, "clear_output", ["wait": false])
             
 
-        try sendStdout(kernel: kernel, stdout:
-                       stdout[((clear_sequence_index + clear_sequence_length)...).pythonObject])
+        try send_stdout(kernel: kernel, stdout:
+                        stdout[((clear_sequence_index + clear_sequence_length)...).pythonObject])
     } else {
         try kernel.send_response.throwing.dynamicallyCall(withArguments:
             kernel.iopub_socket, "stream", ["name": "stdout", "text": stdout])
     }
 }
 
-fileprivate func getAndSendStdout(_ selfRef: PythonObject) throws {
-    let stdout = PythonObject("").join(try getStdout(selfRef))
+fileprivate func get_and_send_stdout(_ selfRef: PythonObject) throws {
+    let stdout = PythonObject("").join(try get_stdout(selfRef))
     
     if Python.len(stdout) > 0 {
         selfRef.had_stdout = true
-        try sendStdout(kernel: selfRef.kernel, stdout: stdout)
+        try send_stdout(kernel: selfRef.kernel, stdout: stdout)
     }
-}
-
-func runStdoutHandler(_ selfRef: PythonObject) -> PythonObject {
-    do {
-        while true {
-            if Bool(selfRef.stop_event.wait(0.1))! {
-                break
-            }
-            
-            try getAndSendStdout(selfRef)
-        }
-        
-        try getAndSendStdout(selfRef)
-    } catch(let e) {
-        selfRef.kernel.log.error("Exception in StdoutHandler: \(Python.str(e.localizedDescription))")
-    }
-    
-    return Python.None
 }
