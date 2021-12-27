f80f5bd0467896b1d85e616962f3df1f277413ab
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 15:59:32 -0500

    Update Completion.swift

--- Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/Completion.swift
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/Completion.swift
@@ -1,26 +1,6 @@
 import Foundation
 import PythonKit
 
-func do_complete(_ kwargs: PythonObject) throws -> PythonObject {
-    let selfRef = kwargs["self"]
-    let cursor_pos = kwargs["cursor_pos"]
-    
-    if !Bool(selfRef.completion_enabled)! {
-        return [
-            "status": "ok",
-            "matches": [],
-            "cursor_start": cursor_pos,
-            "cursor_end": cursor_pos,
-        ]
-    }
-    
-    struct CompletionNotImplementedError: LocalizedError {
-        let errorDescription: String? = "`do_complete` has not been implemented for the Swift Kernel."
-    }
-    
-    throw CompletionNotImplementedError()
-}
-
 func handle_disable_completion(_ selfRef: PythonObject) throws {
     selfRef.completion_enabled = false
     try send_response(selfRef, text: "Completion disabled!\n")
@@ -43,4 +23,24 @@ fileprivate func send_response(_ selfRef: PythonObject, text: String) throws {
         "name": "stdout",
         "text": message
     ])
+}
+
+func do_complete(_ kwargs: PythonObject) throws -> PythonObject {
+    let selfRef = kwargs["self"]
+    let cursor_pos = kwargs["cursor_pos"]
+    
+    if !Bool(selfRef.completion_enabled)! {
+        return [
+            "status": "ok",
+            "matches": [],
+            "cursor_start": cursor_pos,
+            "cursor_end": cursor_pos,
+        ]
+    }
+    
+    struct CompletionNotImplementedError: LocalizedError {
+        let errorDescription: String? = "`do_complete` has not been implemented for the Swift Kernel."
+    }
+    
+    throw CompletionNotImplementedError()
 }
