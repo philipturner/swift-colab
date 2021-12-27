a51222e378a778f33ebbbd5fe1dbe039c2b1b0da
Philip Turner <philipturner.AR@gmail.com>
2021-12-26 20:05:32 -0500

    Update CreateStdoutHandler.swift

--- Sources/SwiftColab/JupyterKernel/CreateStdoutHandler.swift
+++ Sources/SwiftColab/JupyterKernel/CreateStdoutHandler.swift
@@ -1,7 +1,9 @@
 import Foundation
 import PythonKit
 import SwiftPythonBridge
+
 fileprivate let SwiftModule = Python.import("Swift")
+fileprivate let threading = Python.import("threading")
 
 @_cdecl("JKCreateStdoutHandler")
 public func JKCreateStdoutHandler(_ argsRef: OwnedPyObjectPointer) -> OwnedPyObjectPointer {
@@ -11,6 +13,9 @@ public func JKCreateStdoutHandler(_ argsRef: OwnedPyObjectPointer) -> OwnedPyObj
     let args = PythonObject(stdoutHandlerRef)
     let (handler, kernel) = (args[0], args[1])
     
+    handler.kernel = kernel
+    handler.stop_event = threading.Event()
+    handler.had_stdout = false
     
     return SwiftModule.SwiftReturnValue(noneObject, errorObject).ownedPyObject
 }
