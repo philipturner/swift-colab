0c383eb1203010bfe752b4b7d84f92e04b8bf2ed
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 13:13:05 -0500

    Update PreprocessAndExecute.swift

--- Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/PreprocessAndExecute.swift
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/PreprocessAndExecute.swift
@@ -4,3 +4,7 @@ import PythonKit
 func execute(_ selfRef: PythonObject, code: PythonObject) -> ExecutionResult {
     
 }
+
+fileprivate func file_name_for_source_location(_ selfRef: PythonObject) -> String {
+    "<Cell \(selfRef.execution_count)>"
+}
