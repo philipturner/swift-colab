bb92c708cef819a2c8818b9f7413b4e0c7188871
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 12:18:32 -0500

    Create InitSwift.swift

---
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel/InitSwift.swift
@@ -0,0 +1,18 @@
+import Foundation
+import PythonKit
+let lldb = Python.import("lldb")
+
+/// Initializes Swift so that it's ready to start executing user code.
+///
+/// This must happen after package installation, because the ClangImporter
+/// does not see modulemap files that appear after it has started.
+func init_swift(_ selfRef: PythonObject) throws -> PythonObject {
+    try init_repl_process(selfRef)
+    try init_kernel_communicator(selfRef)
+    try init_int_bitwidth(selfRef)
+    try init_sigint_handler(selfRef)
+    
+    // We do completion by default when the toolchain has the SBTarget.CompleteCode API.
+    // The user can disable/enable using "%disableCompletion" and "%enableCompletion".
+    selfRef.completion_enabled = Python.hasattr(selfRef.target, "CompleteCode")
+}
