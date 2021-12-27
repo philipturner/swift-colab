32e024387ff5eea1a3d2de05f227ae0d43d6dfce
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 13:17:41 -0500

    Update PreprocessAndExecute.swift

--- Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/PreprocessAndExecute.swift
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/PreprocessAndExecute.swift
@@ -1,8 +1,23 @@
 import Foundation
 import PythonKit
+fileprivate let lldb = Python.import("lldb")
 
 func execute(_ selfRef: PythonObject, code: PythonObject) -> ExecutionResult {
+    let fileName = file_name_for_source_location(selfRef)
+    let locationDirective = "#sourceLocation(file: \(fileName), line: 1)"
+    let codeWithLocationDirective: PythonObject = "\(locationDirective)\n\(code)"
     
+    let result = selfRef.target.EvaluateExpression(
+        codeWithLocationDirective, selfRef.expr_opts)
+    let errorType = result.error.type
+    
+    if errorType == lldb.eErrorTypeInvalid {
+        return SuccessWithValue(result)
+    } else if errorType == lldb.eErrorTypeGeneric {
+        return SuccessWithoutValue()
+    } else {
+        return SwiftError(result)
+    }
 }
 
 fileprivate func file_name_for_source_location(_ selfRef: PythonObject) -> String {
