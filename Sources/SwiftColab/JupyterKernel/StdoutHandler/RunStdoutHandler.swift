ab2ea6c78052bd3cf09255f8b7be0262005fbb6e
Philip Turner <philipturner.AR@gmail.com>
2021-12-26 20:22:59 -0500

    Update RunStdoutHandler.swift

--- Sources/SwiftColab/JupyterKernel/StdoutHandler/RunStdoutHandler.swift
+++ Sources/SwiftColab/JupyterKernel/StdoutHandler/RunStdoutHandler.swift
@@ -4,3 +4,11 @@ import PythonKit
 func runStdoutHandler(_ selfRef: PythonObject) -> PythonObject {
     Python.None
 }
+
+fileprivate func getAndSendStdout(_ selfRef: PythonObject) throws {
+    
+}
+
+fileprivate func sendStdout(_ selfRef: PythonObject, stdout: PythonObject) throws {
+    
+}
