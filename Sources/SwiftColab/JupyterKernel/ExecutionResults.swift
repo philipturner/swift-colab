994b84db3c38221874ae5e7b8b33bca700236f20
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 11:22:12 -0500

    Update ExecutionResults.swift

--- Sources/SwiftColab/JupyterKernel/ExecutionResults.swift
+++ Sources/SwiftColab/JupyterKernel/ExecutionResults.swift
@@ -1,14 +1,16 @@
 import Foundation
 import PythonKit
 
-protocol ExecutionResult {
-    // Protocol for the result of executing code.
-}
+/// Protocol for the result of executing code.
+protocol ExecutionResult: CustomDebugStringConvertible { }
 
-protocol ExecutionResultSuccess: ExecutionResult {
-    // Protocol for the result of successfully executing code.
-}
+/// Protocol for the result of successfully executing code.
+protocol ExecutionResultSuccess: ExecutionResult { }
 
-protocol ExecutionResultError: ExecutionResult {
-    // Protocol for the result of unsuccessfully executing code.
+/// Protocol for the result of unsuccessfully executing code.
+protocol ExecutionResultError: ExecutionResult { }
+
+/// The code executed successfully, and did not produce a value.
+struct SuccessWithoutValue: ExecutionResultSuccess {
+    
 }
