7e77cbe7ce535762303da746bc1033a4a76a957a
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 11:29:32 -0500

    Update ExecutionErrors.swift

--- Sources/SwiftColab/JupyterKernel/ExecutionErrors.swift
+++ Sources/SwiftColab/JupyterKernel/ExecutionErrors.swift
@@ -1,16 +1,1 @@
-import Foundation
-import PythonKit
 
-struct NotImplementedError: LocalizedError {
-    let errorDescription: String?
-    
-    init(file: String = #file, line: String = #line, function: String = #function) {
-        errorDescription = "Something was not implemented in Swift file: \(file), line: \(line), function: \(function)"
-    }
-}
-
-extension ExecutionResultError {
-    func description() throws -> String {
-        throw NotImplementedError()
-    }
-}
