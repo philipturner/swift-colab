5670a17a6e632f08919eec069ad549862b1da767
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 19:03:17 -0500

    Update ProcessInstalls.swift

--- Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/ProcessInstalls.swift
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/ProcessInstalls.swift
@@ -5,3 +5,12 @@ let os = Python.import("os")
 let re = Python.import("re")
 let shlex = Python.import("shlex")
 let subprocess = Python.import("subprocess")
+
+fileprivate func process_install_location_line(_ selfRef: PythonObject, line: PythonObject) throws -> PythonObject {
+    let regexExpression: PythonObject = ###"""
+    ^\s*%install-location (.*)$
+    """###
+    guard let install_location = Optional(re.match(regexExpression, line)?.group(1) else {
+        return (line, Python.None)
+    }
+}
