aaf463738606fe3be78442a1b87f1d962d7c4bbb
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 21:36:21 -0500

    Update InstallPackages.swift

--- Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/InstallPackages.swift
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/InstallPackages.swift
@@ -1,1 +1,10 @@
+import Foundation
+import PythonKit
 
+func install_packages(_ selfRef: PythonObject,
+                      packages: PythonObject,
+                      swiftpm_flags: PythonObject,
+                      extra_include_commands: PythonObject,
+                      user_install_location: PythonObject) throws {
+  
+}
