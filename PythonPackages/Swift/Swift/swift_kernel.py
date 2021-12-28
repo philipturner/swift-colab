7a3bd90419895d070be869cd524791a1458cd3a2
Philip Turner <philipturner.AR@gmail.com>
2021-12-28 10:38:37 -0500

    Update swift_kernel.py

--- PythonPackages/Swift/Swift/swift_kernel.py
+++ PythonPackages/Swift/Swift/swift_kernel.py
@@ -59,8 +59,11 @@ class SwiftKernel(Kernel):
         })
     
     def lambda1(src_folder):
-        return lambda m: 'header "%s"' %
-            (m.group(1) if os.path.isabs(m.group(1)) else os.path.abspath(os.path.join(src_folder, m.group(1)))) }\""
+        def extract(input):
+            return input
+        return extract(lambda m: 'header "%s"' %
+                          (m.group(1) if os.path.isabs(m.group(1)) else os.path.abspath(os.path.join(src_folder, m.group(1))))
+                      )
                         
 
 if __name__ == "__main__":
