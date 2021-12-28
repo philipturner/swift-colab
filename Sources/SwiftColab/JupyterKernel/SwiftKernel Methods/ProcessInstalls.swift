1dddac85b7ac14aebd333be3a3659c3b81bcb745
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 20:05:45 -0500

    Update ProcessInstalls.swift

--- Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/ProcessInstalls.swift
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/ProcessInstalls.swift
@@ -6,6 +6,25 @@ let re = Python.import("re")
 let shlex = Python.import("shlex")
 let subprocess = Python.import("subprocess")
 
+fileprivate func process_install_substitute(template: inout PythonObject, line_index: PythonObject) throws {
+    do {
+        let function = Python.string.Template(template).substitute.throwing
+        template = try function.dynamicallyCall(withArguments: ["cwd": os.getcwd()])
+    } catch PythonError.exception(let error, let traceback) {
+        let e = PythonError.exception(error, traceback: traceback)
+        
+        if e.__class__ == Python.KeyError {
+            throw PackageInstallException(
+                "Line \(line_index + 1): Invalid template argument \(e)")
+        } else if e.__class__ == Python.ValueError {
+            throw PackageInstallException(
+                "Line \(line_index + 1): \(e)")
+        } else {
+            throw e
+        }
+    }
+}
+
 fileprivate func process_install_location_line(_ selfRef: PythonObject, line_index: PythonObject, line: PythonObject) throws -> PythonObject {
     let regexExpression: PythonObject = ###"""
     ^\s*%install-location (.*)$
@@ -57,5 +76,12 @@ fileprivate func process_install_swiftpm_flags_line(_ selfRef: PythonObject, lin
 }
 
 fileprivate func process_install_line(_ selfRef: PythonObject, line_index: PythonObject, line: PythonObject) throws -> PythonObject {
+    let regexExpression: PythonObject = ###"""
+    ^\s*%install (.*)$
+    """###
+    guard let install_match = Optional(re.match(regexExpression, line)) else {
+        return (line, [])
+    }
     
+    let parsed = 
 }
