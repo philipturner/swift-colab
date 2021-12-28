5cb577772106ab8163fd542ffe15e2c3d0e9d037
Philip Turner <philipturner.AR@gmail.com>
2021-12-27 22:22:28 -0500

    Update InstallPackages.swift

--- Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/InstallPackages.swift
+++ Sources/SwiftColab/JupyterKernel/SwiftKernel Methods/InstallPackages.swift
@@ -2,6 +2,7 @@ import Foundation
 import PythonKit
 
 fileprivate let os = Python.import("os")
+fileprivate let tempfile = Python.import("tempfile")
 
 func install_packages(_ selfRef: PythonObject,
                       packages: [PythonObject],
@@ -24,9 +25,18 @@ func install_packages(_ selfRef: PythonObject,
             "SWIFT_BUILD_PATH is not specified.")
     }
     
-    guard let swift_build_path = os.environ.get("SWIFT_BUILD_PATH") else {
+    guard let swift_package_path = os.environ.get("SWIFT_PACKAGE_PATH") else {
         throw PackageInstallException(
             "Install Error: Cannot install packages because " +
-            "SWIFT_BUILD_PATH is not specified.")
+            "SWIFT_PACKAGE_PATH is not specified.")
     }
+    
+    var package_install_scratchwork_base = tempfile.mkdtemp()
+    package_install_scratchwork_base = os.path.join(package_install_scratchwork_base, "swift-install")
+    
+    let swift_module_search_path = os.path.join(package_install_scratchwork_base, "modules")
+    selfRef.swift_module_search_path = swift_module_search_path
+    
+    let scratchwork_base_path = os.path.dirname(swift_module_search_path)
+    let package_base_path = os.path.join(scratchwork_base_path, "package")
 }
