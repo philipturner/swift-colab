a8319e3d8bf0ad6be7aa4f065e0dca005b5c54cd
Philip Turner <philipturner.AR@gmail.com>
2021-12-29 17:51:14 -0500

    Update __init__.py

--- PythonPackages/Swift/Swift/__init__.py
+++ PythonPackages/Swift/Swift/__init__.py
@@ -73,3 +73,47 @@ class StdoutHandler(threading.Thread):
     
     def run(self):
         self.swift_delegate.call("run", self)
+
+# From swifts_shell
+
+from ipykernel.zmqshell import ZMQInteractiveShell
+from jupyter_client.session import Session
+
+
+class CapturingSocket:
+    """Simulates a ZMQ socket, saving messages instead of sending them.
+    We use this to capture display messages.
+    """
+
+    def __init__(self):
+        self.messages = []
+
+    def send_multipart(self, msg, **kwargs):
+        self.messages.append(msg)
+
+
+class SwiftShell(ZMQInteractiveShell):
+    """An IPython shell, modified to work within Swift."""
+
+    def enable_gui(self, gui):
+        """Disable the superclass's `enable_gui`.
+        `enable_matplotlib("inline")` calls this method, and the superclass's
+        method fails because it looks for a kernel that doesn't exist. I don't
+        know what this method is supposed to do, but everything seems to work
+        after I disable it.
+        """
+        pass
+
+    
+def create_shell(username, session_id, key):
+    """Instantiates a CapturingSocket and SwiftShell and hooks them up.
+    
+    After you call this, the returned CapturingSocket should capture all
+    IPython display messages.
+    """
+    socket = CapturingSocket()
+    session = Session(username=username, session=session_id, key=key)
+    shell = SwiftShell.instance()
+    shell.display_pub.session = session
+    shell.display_pub.pub_socket = socket
+    return (socket, shell)
