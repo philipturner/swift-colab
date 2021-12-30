3e7d47527cd453d4295ecc069469866adebea525
Philip Turner <philipturner.AR@gmail.com>
2021-12-29 23:05:53 -0500

    Update EnableJupyterDisplay.swift

--- Sources/SwiftColab/include/EnableJupyterDisplay.swift
+++ Sources/SwiftColab/include/EnableJupyterDisplay.swift
@@ -149,8 +149,8 @@ extension JupyterDisplay {
     private static func updateParentMessage(to parentMessage: KernelCommunicator.ParentMessage) {
         do {
             let jsonData = (parentMessage.json).data(using: .utf8, allowLossyConversion: false)
-            let jsonDict = try JSONSerialization.jsonObject(with: jsonData!) as? NSDictionary
-            let headerData = try JSONSerialization.data(withJSONObject: jsonDict!["header"],
+            let jsonDict = try JSONSerialization.jsonObject(with: jsonData!) as! NSDictionary
+            let headerData = try JSONSerialization.data(withJSONObject: jsonDict["header"],
                                                         options: .prettyPrinted)
             parentHeader = String(data: headerData, encoding: .utf8)!
         } catch {
