40b5fc98e309c2f34e4b2a9b9eb162655635146d
Philip Turner <philipturner.AR@gmail.com>
2021-12-29 20:35:45 -0500

    Update install_swift.swift

--- install_swift.swift
+++ install_swift.swift
@@ -86,20 +86,6 @@ try fm.createDirectory(atPath: "/opt/swift/tmp", withIntermediateDirectories: tr
 try fm.createDirectory(atPath: "/opt/swift/lib", withIntermediateDirectories: true)
 try fm.createDirectory(atPath: "/opt/swift/packages", withIntermediateDirectories: true)
 
-// Not installing Backtrace because I don't see it helping anything.
-
-/*
-// Install philipturner/swift-backtrace
-try doCommand(["swift", "build"], directory: "/opt/swift/packages/swift-backtrace")
-let backtraceProductsPath = "/opt/swift/packages/swift-backtrace/.build/debug"
-try doCommand(["swiftc", "/opt/swift/swift-colab/Sources/SwiftColab/InstallBacktrace.swift",
-               "-L", backtraceProductsPath, "-lBacktrace",
-               "-I", backtraceProductsPath,
-               "-Onone"],
-               directory: "/opt/swift/tmp")
-try doCommand(["/opt/swift/tmp/InstallBacktrace"])
-*/
-
 // Install philipturner/PythonKit
 let pythonKitProductsPath = "/opt/swift/packages/PythonKit/.build/debug"
 let pythonKitLibPath = "/opt/swift/lib/libPythonKit.so"
