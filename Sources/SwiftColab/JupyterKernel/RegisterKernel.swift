9ddb357a18d583c726206e5b5eb5c44613883f95
Philip Turner <philipturner.AR@gmail.com>
2022-01-03 21:34:52 -0500

    Update RegisterKernel.swift

--- Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
+++ Sources/SwiftColab/JupyterKernel/RegisterKernel.swift
@@ -81,7 +81,7 @@ fileprivate func make_kernel_env() -> PythonObject {
     let swift_toolchain = "/opt/swift/toolchain"
     
     let kernel_env: PythonObject = [:]
-//     kernel_env["PYTHONPATH"] = .init("\(swift_toolchain)/usr/lib/python3/dist-packages")
+    kernel_env["PYTHONPATH"] = .init("\(swift_toolchain)/usr/lib/python3/dist-packages")
     kernel_env["LD_LIBRARY_PATH"] = .init("\(swift_toolchain)/usr/lib/swift/linux")
     kernel_env["REPL_SWIFT_PATH"] = .init("\(swift_toolchain)/usr/bin/repl_swift")
     kernel_env["SWIFT_BUILD_PATH"] = .init("\(swift_toolchain)/usr/bin/swift-build")
@@ -100,9 +100,9 @@ fileprivate struct Exception: LocalizedError {
 
 /// Validates that the env vars refer to things that actually exist.
 fileprivate func validate_kernel_env(_ kernel_env: PythonObject) throws {
-//     guard Bool(os.path.isfile(kernel_env["PYTHONPATH"] + "/lldb/_lldb.so"))! else {
-//         throw Exception("lldb python libs not found at \(kernel_env["PYTHONPATH"])")
-//     }
+    guard Bool(os.path.isfile(kernel_env["PYTHONPATH"] + "/lldb/_lldb.so"))! else {
+        throw Exception("lldb python libs not found at \(kernel_env["PYTHONPATH"])")
+    }
     
     guard Bool(os.path.isfile(kernel_env["REPL_SWIFT_PATH"]))! else {
         throw Exception("repl_swift binary not found at \(kernel_env["REPL_SWIFT_PATH"])")
