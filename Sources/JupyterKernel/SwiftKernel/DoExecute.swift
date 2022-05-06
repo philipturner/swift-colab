601910a8220d7cf0123c6ec3da9e963a5816f056
Philip Turner <philipturner.AR@gmail.com>
2022-05-06 12:47:43 -0400

    Update DoExecute.swift

--- Sources/JupyterKernel/SwiftKernel/DoExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/DoExecute.swift
@@ -3,10 +3,9 @@ fileprivate let json = Python.import("json")
 fileprivate let jsonutil = Python.import("jupyter_client").jsonutil
 
 func doExecute(code: String) throws -> PythonObject? {
-  // TODO: make this happen in the very first cell, regardless of
-  // whether it's whitespace. In fact, just remove the check for 
-  // whether the code is blank. Try to initialize Swift in the
-  // kernel object's initializer.
+  // TODO(v2.1): Make this happen in the very first cell, regardless of whether
+  // it's whitespace. In fact, just remove the check for whether the code is 
+  // blank. Try to initialize Swift in the kernel object's initializer.
   if !KernelContext.debuggerInitialized {
     try initSwift()
     KernelContext.debuggerInitialized = true
@@ -16,8 +15,8 @@ func doExecute(code: String) throws -> PythonObject? {
   let stdoutHandler = StdoutHandler()
   stdoutHandler.start()
   
-  // Execute the cell, handle unexpected exceptions, and make sure to
-  // always clean up the stdout handler.
+  // Execute the cell, handle unexpected exceptions, and make sure to always 
+  // clean up the stdout handler.
   var result: ExecutionResult
   do {
     defer {
@@ -62,25 +61,23 @@ func doExecute(code: String) throws -> PythonObject? {
       traceback = ["Process killed"]
       sendIOPubErrorMessage(traceback)
       
-      // Exit the kernel because there is no way to recover from a
-      // killed process. The UI will tell the user that the kernel has
-      // died and the UI will automatically restart the kernel.
-      // We do the exit in a callback so that this execute request can
-      // cleanly finish before the kernel exits.
+      // Exit the kernel because there is no way to recover from a killed 
+      // process. The UI will tell the user that the kernel has died and the UI 
+      // will automatically restart the kernel. We do the exit in a callback so 
+      // that this execute request can cleanly finish before the kernel exits.
       let loop = Python.import("ioloop").IOLoop.current()
       loop.add_timeout(Python.import("time").time() + 0.1, loop.stop)
     } else if Bool(stdoutHandler.had_stdout)! {
-      // When there is stdout, it is a runtime error. Stdout, which we
-      // have already sent to the client, contains the error message
-      // (plus some other ugly traceback that we should eventually
-      // figure out how to suppress), so this block of code only needs
-      // to add a traceback.
+      // When there is stdout, it is a runtime error. Stdout, which we have
+      // already sent to the client, contains the error message (plus some other 
+      // ugly traceback that we should eventually figure out how to suppress), 
+      // so this block of code only needs to add a traceback.
       traceback = ["Current stack trace:"]
       traceback += try prettyPrintStackTrace()
       sendIOPubErrorMessage(traceback)      
     } else {
-      // There is no stdout, so it must be a compile error. Simply return
-      // the error without trying to get a stack trace.
+      // There is no stdout, so it must be a compile error. Simply return the 
+      // error without trying to get a stack trace.
       traceback = [result.description]
       sendIOPubErrorMessage(traceback)
     }
