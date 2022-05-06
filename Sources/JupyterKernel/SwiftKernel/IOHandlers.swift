1101bf4fd8f7c1f81faa58be5164348415c30d6d
Philip Turner <philipturner.AR@gmail.com>
2022-05-06 18:32:34 -0400

    Update IOHandlers.swift

--- Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
+++ Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
@@ -43,41 +43,45 @@ let SIGINTHandler = PythonClass(
   ]
 ).pythonObject
 
-let StdoutHandler = PythonClass(
-  "StdoutHandler",
-  superclasses: [threading.Thread],
-  members: [
-    "__init__": PythonInstanceMethod { (`self`: PythonObject) in
-      threading.Thread.__init__(`self`)
-      `self`.daemon = true
-      `self`.stop_event = threading.Event()
-      `self`.had_stdout = false
-      return Python.None
-    },
+// let StdoutHandler = PythonClass(
+//   "StdoutHandler",
+//   superclasses: [threading.Thread],
+//   members: [
+//     "__init__": PythonInstanceMethod { (`self`: PythonObject) in
+//       threading.Thread.__init__(`self`)
+//       `self`.daemon = true
+//       `self`.stop_event = threading.Event()
+//       `self`.had_stdout = false
+//       return Python.None
+//     },
     
-    "run": PythonInstanceMethod { (`self`: PythonObject) in
-       globalMessages.append("hello world 4")
-       updateProgressFile()
-       while true {
-         globalMessages.append("hello world 5")
-         updateProgressFile()
-         if Bool(`self`.stop_event.wait(timeout: 0.1))! { //== true {
-           globalMessages.append("hello world 6")
-           updateProgressFile()
-           break
-         } else {
-           globalMessages.append("hello world 6.2")
-           updateProgressFile()
-         }
-         getAndSendStdout(handler: `self`)
-       }
-       globalMessages.append("hello world 7")
-       updateProgressFile()           
-       getAndSendStdout(handler: `self`)
-       return Python.None
-    }
-  ]
-).pythonObject
+//     "run": PythonInstanceMethod { (`self`: PythonObject) in
+//        globalMessages.append("hello world 4")
+//        updateProgressFile()
+//        while true {
+//          globalMessages.append("hello world 5")
+//          updateProgressFile()
+//          if Bool(`self`.stop_event.wait(timeout: 0.1))! { //== true {
+//            globalMessages.append("hello world 6")
+//            updateProgressFile()
+//            break
+//          } else {
+//            globalMessages.append("hello world 6.2")
+//            updateProgressFile()
+//          }
+//          getAndSendStdout(handler: `self`)
+//        }
+//        globalMessages.append("hello world 7")
+//        updateProgressFile()           
+//        getAndSendStdout(handler: `self`)
+//        return Python.None
+//     }
+//   ]
+// ).pythonObject
+
+internal actor StdoutHandler {
+  
+}
 
 fileprivate var cachedScratchBuffer: UnsafeMutablePointer<CChar>?
 
@@ -93,48 +97,48 @@ fileprivate func getStdout() -> String {
     guard stdoutSize > 0 else {
       break
     }
-//     let stdoutSegment = Data(
-//       bytesNoCopy: scratchBuffer, count: Int(stdoutSize), deallocator: .none)
     let stdoutSegment = Data(
-      bytes: scratchBuffer, count: Int(stdoutSize))
+      bytesNoCopy: scratchBuffer, count: Int(stdoutSize), deallocator: .none)
+//     let stdoutSegment = Data(
+//       bytes: scratchBuffer, count: Int(stdoutSize))
     stdout += stdoutSegment
   }
   return String(data: stdout, encoding: .utf8)!
 }
 
 fileprivate func sendStdout(_ stdout: PythonObject /* String */) {
-//   let kernel = KernelContext.kernel
-//   if let range = stdout.range(of: "\033[2J") {
-//     sendStdout(String(stdout[..<range.lowerBound]))
-//     kernel.send_response(kernel.iopub_socket, "clear_output", [
-//       "wait": false
-//     ])
-//     sendStdout(String(stdout[range.upperBound...]))
-//   } else {
-//     kernel.send_response(kernel.iopub_socket, "stream", [
-//       "name": "stdout",
-//       "text": stdout
-//     ])
-//   }
-  
   let kernel = KernelContext.kernel
+  if let range = stdout.range(of: "\033[2J") {
+    sendStdout(String(stdout[..<range.lowerBound]))
+    kernel.send_response(kernel.iopub_socket, "clear_output", [
+      "wait": false
+    ])
+    sendStdout(String(stdout[range.upperBound...]))
+  } else {
+    kernel.send_response(kernel.iopub_socket, "stream", [
+      "name": "stdout",
+      "text": stdout
+    ])
+  }
+  
+//   let kernel = KernelContext.kernel
                             
-    let clear_sequence: PythonObject = "\033[2J"
-    let clear_sequence_index = stdout.find(clear_sequence)
-    let clear_sequence_length = Python.len(clear_sequence)
+//     let clear_sequence: PythonObject = "\033[2J"
+//     let clear_sequence_index = stdout.find(clear_sequence)
+//     let clear_sequence_length = Python.len(clear_sequence)
     
-    if clear_sequence_index != -1 {
-        sendStdout(stdout[(..<clear_sequence_index).pythonObject])
+//     if clear_sequence_index != -1 {
+//         sendStdout(stdout[(..<clear_sequence_index).pythonObject])
         
-        try! kernel.send_response.throwing.dynamicallyCall(withArguments:
-            kernel.iopub_socket, "clear_output", ["wait": false])
+//         try! kernel.send_response.throwing.dynamicallyCall(withArguments:
+//             kernel.iopub_socket, "clear_output", ["wait": false])
             
 
-        sendStdout(stdout[((clear_sequence_index + clear_sequence_length)...).pythonObject])
-    } else {
-         try! kernel.send_response.throwing.dynamicallyCall(withArguments:
-            kernel.iopub_socket, "stream", ["name": "stdout", "text": stdout])
-    }
+//         sendStdout(stdout[((clear_sequence_index + clear_sequence_length)...).pythonObject])
+//     } else {
+//          try! kernel.send_response.throwing.dynamicallyCall(withArguments:
+//             kernel.iopub_socket, "stream", ["name": "stdout", "text": stdout])
+//     }
 }
 
 fileprivate func getAndSendStdout(handler: PythonObject) {
