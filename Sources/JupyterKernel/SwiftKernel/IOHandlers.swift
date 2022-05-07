7774527bca992ee63bb65c4ed82d76d787f0edb3
Philip Turner <philipturner.AR@gmail.com>
2022-05-07 13:45:51 -0400

    Update IOHandlers.swift

--- Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
+++ Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
@@ -36,7 +36,7 @@ class StdoutHandler {
   
   init() {
     DispatchQueue.global(qos: .userInteractive).async { [self] in
-//       var previousDate = Date()
+      var previousDate = Date()
       while true {
         usleep(100_000)
         if shouldStop {
@@ -44,10 +44,10 @@ class StdoutHandler {
         }
         // TODO: time how long this operation takes
         getAndSendStdout(hadStdout: &hadStdout)
-//         let currentDate = Date()
-//         let elapsedTime = currentDate.timeIntervalSince(previousDate) * 1e6
-//         KernelContext.log("Stdout retrieval time: \(elapsedTime)")
-//         previousDate = currentDate
+        let currentDate = Date()
+        let elapsedTime = currentDate.timeIntervalSince(previousDate) * 1e6
+        KernelContext.log("Stdout retrieval time: \(elapsedTime)")
+        previousDate = currentDate
       }
       getAndSendStdout(hadStdout: &hadStdout)
       semaphore.signal()
