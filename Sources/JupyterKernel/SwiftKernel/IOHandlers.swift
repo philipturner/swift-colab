bfa868df718ed4a2fea7fb3e44c56d7a2b20b440
Philip Turner <philipturner.AR@gmail.com>
2022-05-06 20:29:04 -0400

    Update IOHandlers.swift

--- Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
+++ Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
@@ -41,8 +41,7 @@ let SIGINTHandler = PythonClass(
         updateProgressFile()
         
         if vulnerableProcess != Python.None {
-          vulnerableProcess.send_signal(signal.SIGKILL)
-//           vulnerableProcess.terminate()
+          vulnerableProcess.send_signal(signal.SIGSEGV)
           killedVulnerableProcess = true
           globalMessages.append("hello world 2.2")
         } else {
