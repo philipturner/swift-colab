29b6fb0426cad8ef002a27357488b8b561ffe898
Philip Turner <philipturner.AR@gmail.com>
2022-05-08 17:47:25 -0400

    Update PreprocessAndExecute.swift

--- Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
@@ -17,11 +17,9 @@ func preprocessAndExecute(
     let executionCount = Int(KernelContext.kernel.execution_count)!
     
     DispatchQueue.global().async {
-//       KernelContext.lldbQueue.sync {
-        executionResult = execute(
-          code: preprocessed, lineIndex: isCell ? 0 : nil, 
-          executionCount: executionCount)
-//       }
+      executionResult = execute(
+        code: preprocessed, lineIndex: isCell ? 0 : nil, 
+        executionCount: executionCount)
     }
     
     while executionResult == nil {
