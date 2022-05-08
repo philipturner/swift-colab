8a6177a4ef8bee6cf9bdec21d3d2655ca826c222
Philip Turner <philipturner.AR@gmail.com>
2022-05-08 17:44:15 -0400

    Update InitSwift.swift

--- Sources/JupyterKernel/SwiftKernel/InitSwift.swift
+++ Sources/JupyterKernel/SwiftKernel/InitSwift.swift
@@ -27,9 +27,9 @@ fileprivate struct CEnvironment {
 fileprivate var sigintHandler: PythonObject!
 
 func initSwift() throws {
-  try KernelContext.lldbQueue.sync {
+//   try KernelContext.lldbQueue.sync {
     try initReplProcess()
-  }
+//   }
   KernelContext.log("finished initReplProcess")
   try initKernelCommunicator()
   KernelContext.log("finished initKernelCommunicator")
