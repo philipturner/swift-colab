924d1e30ff9558c539b0f50a817c753523f987fd
Philip Turner <philipturner.AR@gmail.com>
2022-05-07 21:37:57 -0400

    Update KernelContext.swift

--- Sources/JupyterKernel/KernelContext.swift
+++ Sources/JupyterKernel/KernelContext.swift
@@ -34,69 +34,39 @@ struct KernelContext {
     kernel.send_response(kernel.iopub_socket, header, message)
   }
   
-  // Allows sending responses from other threads, preventing multithreaded
-  // access to Python that violates the GIL and (maybe) makes the UI
-  // unresponsive.
-  private static var cachedResponses: [(String, PythonConvertible)] = []
-  static var responseQueue = DispatchQueue(
-    label: "com.philipturner.swift-colab.KernelContext.responseQueue")
-  
-  // Must call this on `responseQueue`; `message` must not contain any Python 
-  // objects.
-  static func sendAsyncResponse(
-    _ header: String, _ message: PythonConvertible
-  ) {
-    cachedResponses.append((header, message))
-  }
-  
-  static func flushResponses() {
-    let responses = responseQueue.sync { () -> [(String, PythonConvertible)] in
-      let output = cachedResponses
-      cachedResponses = []
-      return output
-    }
-    
-    let send_response = kernel.send_response
-    let iopub_socket = kernel.iopub_socket
-    for response in responses {
-      send_response(iopub_socket, response.0, response.1)
-//       KernelContext.log("a flushed response")
-    }
-  }
-  
-  // Dynamically loaded LLDB bringing functions
-  
-  // Prevents data races when accessing LLDB
+  // Prevents data races when accessing LLDB, which caused crashes previously.
   static let lldbQueue = DispatchQueue(
     label: "com.philipturner.swift-colab.KernelContext.lldbQueue")
   
-  static let _1_init_repl_process: @convention(c) (
+  // Dynamically loaded LLDB bringing functions
+  
+  static let init_repl_process: @convention(c) (
     OpaquePointer, UnsafePointer<CChar>) -> Int32 = 
     LLDBProcessLibrary.loadSymbol(name: "init_repl_process")
   
-  static let _1_execute: @convention(c) (
+  static let execute: @convention(c) (
     UnsafePointer<CChar>, 
     UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "execute")
   
-  static let _1_process_is_alive: @convention(c) (
+  static let process_is_alive: @convention(c) (
     UnsafeMutablePointer<Int32>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "process_is_alive")
   
-  static let _1_after_successful_execution: @convention(c) (
+  static let after_successful_execution: @convention(c) (
     UnsafeMutablePointer<UnsafeMutablePointer<UInt64>?>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "after_successful_execution")
   
-  static let _1_get_stdout: @convention(c) (
+  static let get_stdout: @convention(c) (
     UnsafeMutablePointer<CChar>, Int32) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "get_stdout")
   
-  static let _1_get_pretty_stack_trace: @convention(c) (
+  static let get_pretty_stack_trace: @convention(c) (
     UnsafeMutablePointer<UnsafeMutablePointer<UnsafeMutablePointer<CChar>>?>,
     UnsafeMutablePointer<Int32>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "get_pretty_stack_trace")
   
-  static let _1_async_interrupt_process: @convention(c) () -> Int32 =
+  static let async_interrupt_process: @convention(c) () -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "async_interrupt_process")
 }
 
