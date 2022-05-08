c9dfdd9f1acf463981b1011d04f9a2368d7fb0d5
Philip Turner <philipturner.AR@gmail.com>
2022-05-07 20:51:12 -0400

    Update KernelContext.swift

--- Sources/JupyterKernel/KernelContext.swift
+++ Sources/JupyterKernel/KernelContext.swift
@@ -66,33 +66,37 @@ struct KernelContext {
   
   // Dynamically loaded LLDB bringing functions
   
-  static let init_repl_process: @convention(c) (
+  // Prevents data races when accessing LLDB
+  static let lldbQueue = DispatchQueue(
+    label: "com.philipturner.swift-colab.KernelContext.lldbQueue")
+  
+  static let _1_init_repl_process: @convention(c) (
     OpaquePointer, UnsafePointer<CChar>) -> Int32 = 
     LLDBProcessLibrary.loadSymbol(name: "init_repl_process")
   
-  static let execute: @convention(c) (
+  static let _1_execute: @convention(c) (
     UnsafePointer<CChar>, 
     UnsafeMutablePointer<UnsafeMutablePointer<CChar>?>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "execute")
   
-  static let process_is_alive: @convention(c) (
+  static let _1_process_is_alive: @convention(c) (
     UnsafeMutablePointer<Int32>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "process_is_alive")
   
-  static let after_successful_execution: @convention(c) (
+  static let _1_after_successful_execution: @convention(c) (
     UnsafeMutablePointer<UnsafeMutablePointer<UInt64>?>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "after_successful_execution")
   
-  static let get_stdout: @convention(c) (
+  static let _1_get_stdout: @convention(c) (
     UnsafeMutablePointer<CChar>, Int32) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "get_stdout")
   
-  static let get_pretty_stack_trace: @convention(c) (
+  static let _1_get_pretty_stack_trace: @convention(c) (
     UnsafeMutablePointer<UnsafeMutablePointer<UnsafeMutablePointer<CChar>>?>,
     UnsafeMutablePointer<Int32>) -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "get_pretty_stack_trace")
   
-  static let async_interrupt_process: @convention(c) () -> Int32 =
+  static let _1_async_interrupt_process: @convention(c) () -> Int32 =
     LLDBProcessLibrary.loadSymbol(name: "async_interrupt_process")
 }
 
