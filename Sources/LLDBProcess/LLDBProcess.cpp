f8687b1bfa5fff7725ec14f293978e466fb46457
Philip Turner <philipturner.AR@gmail.com>
2022-05-08 20:48:00 -0400

    Update LLDBProcess.cpp

--- Sources/LLDBProcess/LLDBProcess.cpp
+++ Sources/LLDBProcess/LLDBProcess.cpp
@@ -29,9 +29,10 @@ int init_repl_process(const char **repl_env,
     "settings append target.swift-module-search-paths "
     "/opt/swift/install_location/modules");
   
-//   FILE *errFilePointer = fopen("/opt/swift/err", "w");
-//   debugger.SetErrorFileHandle(errFilePointer, true);
-  debugger.HandleCommand("settings set target.output-path /opt/swift/err");
+  // Redirect stderr to something that Swift-Colab can manually process. This
+  // suppresses the ugly backtraces that appear in stdout.
+  FILE *errFilePointer = fopen("/opt/swift/err", "w");
+  debugger.SetErrorFileHandle(errFilePointer, true);
   
   // LLDB will not crash when scripting because this isn't macOS. However,
   // disabling scripting could decrease startup time if the debugger needs to
