6966e4733a8317b04a7c48e0bb635c08859a4bfd
Philip Turner <philipturner.AR@gmail.com>
2022-05-09 15:52:44 -0400

    Update IOHandlers.swift

--- Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
+++ Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
@@ -115,5 +115,9 @@ func getStderr() -> String {
   let stderr = String(data: stderrData, encoding: .utf8)!
   // TODO: find a way to flush the file without LLDB's appending
   // mechanism messing up
+  precondition(
+    fm.createFile(atPath: "/opt/swift/err", contents: Data()),
+    "Could not write to stderr file for the Swift interpreter")
+  
   return stderr
 }
