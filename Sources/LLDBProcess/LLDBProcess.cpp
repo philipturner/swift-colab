296a874d353d55186ffffc7a7efb0275a5c25c5b
Philip Turner <philipturner.AR@gmail.com>
2022-05-10 09:01:23 -0400

    Update LLDBProcess.cpp

--- Sources/LLDBProcess/LLDBProcess.cpp
+++ Sources/LLDBProcess/LLDBProcess.cpp
@@ -29,7 +29,6 @@ int init_repl_process(const char **repl_env,
   debugger.SetUseColor(true);
   debugger.HandleCommand("settings set use-color true");
   debugger.HandleCommand("settings set target.run-args -color-diagnostics");
-  debugger.HandleCommand("settings set target.env-vars MYVAR=uiui");
   debugger.HandleCommand(
     "settings append target.swift-module-search-paths "
     "/opt/swift/install_location/modules");
