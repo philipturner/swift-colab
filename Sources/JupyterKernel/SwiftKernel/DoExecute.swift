3ebb4acbf1e77150152a4170508826fbbd0dba81
Philip Turner <philipturner.AR@gmail.com>
2022-05-09 20:44:07 -0400

    Update DoExecute.swift

--- Sources/JupyterKernel/SwiftKernel/DoExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/DoExecute.swift
@@ -5,8 +5,7 @@ fileprivate let jsonutil = Python.import("jupyter_client").jsonutil
 func doExecute(code: String) throws -> PythonObject? {
   KernelContext.isInterrupted = false
   KernelContext.pollingStdout = true
-  KernelContext.log("")
-  KernelContext.log("code: \(code)")
+  KernelContext.log("\n" + "code: \(code)")
   
   // Flush stderr
   _ = getStderr(readData: false)
