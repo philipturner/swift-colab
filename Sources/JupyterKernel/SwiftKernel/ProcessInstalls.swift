0af8755b1973a66855877955d5c3695d6e3cb2ac
Philip Turner <philipturner.AR@gmail.com>
2022-05-12 11:04:19 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -386,7 +386,7 @@ fileprivate func processInstall(
       }
     }
     output.append(temp)
-    return makeBlue(output)
+    return output
   }
 
   let swiftBuildPath = "/opt/swift/toolchain/usr/bin/swift-build"
