b7b815fde6639f39ed18ac0402c3cb0a4b09fd56
Philip Turner <philipturner.AR@gmail.com>
2022-05-12 08:05:11 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -371,7 +371,7 @@ fileprivate func processInstall(
   let swiftBuildPath = "/opt/swift/toolchain/usr/bin/swift-build"
   let buildReturnCode = try runTerminalProcess(
     args: [swiftBuildPath] + swiftPMFlags, cwd: packagePath, 
-    filter: removeJSONBlob)
+    filterStdout: removeJSONBlob)
 ////////////////////////////////////////////////////////////////////////////////
 //   let buildProcess = subprocess.Popen(
 //     [swiftBuildPath] + swiftPMFlags,
