e4a4a54ed416289273dc01ed40993865a254b08e
Philip Turner <philipturner.AR@gmail.com>
2022-05-12 08:04:21 -0400

    Update HandleIO.swift

--- Sources/JupyterKernel/SwiftKernel/HandleIO.swift
+++ Sources/JupyterKernel/SwiftKernel/HandleIO.swift
@@ -141,7 +141,9 @@ func getStderr(readData: Bool) -> String? {
 // This attempts to replicate the code located at:
 // https://github.com/ipython/ipython/blob/master/IPython/utils/_process_posix.py,
 //   def system(self, cmd):
-func runTerminalProcess(args: [String], cwd: String? = nil) throws -> Int {
+func runTerminalProcess(
+  args: [String], cwd: String? = nil, proce
+) throws -> Int {
   let joinedArgs = args.joined(separator: " ")
   let process = pexpect.spawn("/bin/sh", args: ["-c", joinedArgs], cwd: cwd)
   let flush = sys.stdout.flush
