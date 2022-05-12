a9723cc3297c9784b5db9ae186ec36bde4fba9aa
Philip Turner <philipturner.AR@gmail.com>
2022-05-12 13:53:08 -0400

    Update RegisterKernel.swift

--- Sources/JupyterKernel/RegisterKernel.swift
+++ Sources/JupyterKernel/RegisterKernel.swift
@@ -11,29 +11,30 @@ public func JupyterKernel_registerSwiftKernel() {
   
   // Cannot be a Swift script because that causes a crash.
   let pythonScript = """
-  from ctypes import PyDLL
-  if __name__ == "__main__":
-      PyDLL("/opt/swift/lib/libJupyterKernel.so").JupyterKernel_createSwiftKernel()
-  """
+    from ctypes import PyDLL
+    if __name__ == "__main__":
+        PyDLL("/opt/swift/lib/libJupyterKernel.so").JupyterKernel_createSwiftKernel()
+    """
   
   let swiftKernelPath = "\(jupyterKernelFolder)/swift_kernel"
   try? fm.removeItem(atPath: swiftKernelPath)
-  fm.createFile(atPath: swiftKernelPath, contents: pythonScript.data(using: .utf8)!)
+  fm.createFile(
+    atPath: swiftKernelPath, contents: pythonScript.data(using: .utf8)!)
   
-  // Create kernel spec
+  // Create kernel spec.
   let kernelSpec = """
-  {
-    "argv": [
-      "/usr/bin/python3",
-      "\(swiftKernelPath)",
-      "-f",
-      "{connection_file}"
-    ],
-    "display_name": "Swift",
-    "language": "swift",
-    "env": {}
-  }
-  """
+    {
+      "argv": [
+        "/usr/bin/python3",
+        "\(swiftKernelPath)",
+        "-f",
+        "{connection_file}"
+      ],
+      "display_name": "Swift",
+      "language": "swift",
+      "env": {}
+    }
+    """
   
   let kernelSpecPath = "\(jupyterKernelFolder)/kernel.json"
   try? fm.removeItem(atPath: kernelSpecPath)
@@ -41,13 +42,11 @@ public func JupyterKernel_registerSwiftKernel() {
   fm.createFile(atPath: kernelSpecPath, contents: kernelSpec.data(using: .utf8)!)
   KernelSpecManager().install_kernel_spec(jupyterKernelFolder, "swift")
   
-  // Overwrite Python kernel script
-  
+  // Overwrite Python kernel script.
   let activeKernelPath = String(ipykernel_launcher.__file__)!
-  
   if !fm.contentsEqual(atPath: swiftKernelPath, andPath: activeKernelPath) {
-      try! fm.copyItem(atPath: swiftKernelPath, toPath: activeKernelPath)
+    try! fm.copyItem(atPath: swiftKernelPath, toPath: activeKernelPath)
   } else {
-      print("Swift Jupyter kernel was already registered")
+    print("Swift Jupyter kernel was already registered")
   }
 }
