a7d26111c52988be89d69726fb0f651192e10047
Philip Turner <philipturner.AR@gmail.com>
2022-05-12 17:32:11 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -560,11 +560,21 @@ func removeJSONBlob(_ line: String) -> String? {
   for char in line.utf8.map(Unicode.Scalar.init).map(Character.init) {
     defer { previousChar = char }
     if char == "\r" || char == "\n" {
+      func getAppended() -> String {
+        var copy = temp
+        copy.append(char)
+        return copy
+      }
       var appendedThisLine = false
+      
       if temp.first == "{" {
         insideBraces = true
       } else if temp.first == "}" {
         insideBraces = false
+      } else if getAppended() == "\u{001B}[2K\r" {
+        output.append("\u{001B}[2K\r")
+        temp.removeAll(keepingCapacity: true)
+        continue
       } else if !insideBraces {
         if Int(temp) == nil {
           if previousChar == "\r" && !appendedPreviousLine {
