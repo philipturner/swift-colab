10a18d5df6cb5ec0f13f33d01cb745613b15b285
Philip Turner <philipturner.AR@gmail.com>
2022-05-13 18:04:25 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -231,6 +231,9 @@ fileprivate func readClangModules() {
 fileprivate func processInstall(
   restOfLine: String, lineIndex: Int
 ) throws {
+  _ = try runTerminalProcess(args: ["cd /opt/swift/packages/1 && /opt/swift/toolchain/usr/bin/swift-build"])
+  return
+  
   let parsed = try shlexSplit(lineIndex: lineIndex, line: restOfLine)
   if parsed.count < 2 {
     throw PreprocessorException(lineIndex: lineIndex, message:
@@ -343,8 +346,7 @@ fileprivate func processInstall(
   // Ask SwiftPM to build the package.
   
   let swiftBuildPath = "/opt/swift/toolchain/usr/bin/swift-build"
-  _ = try runTerminalProcess(args: ["cd /opt/swift/packages/1 && /opt/swift/toolchain/usr/bin/swift-build"])
-  return
+  
   
 //   _ = try runTerminalProcess(args:
 //     ["cd \(packagePath) && \(swiftBuildPath)"] + swiftPMFlags
