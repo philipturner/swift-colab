e9c607b9fb9f00d124b114a089c1e2c1a9283d57
Philip Turner <philipturner.AR@gmail.com>
2022-06-04 10:25:29 -0400

    Update SwiftShell.swift

--- Sources/JupyterKernel/SwiftShell.swift
+++ Sources/JupyterKernel/SwiftShell.swift
@@ -84,9 +84,9 @@ fileprivate let SwiftShell = PythonClass(
       if gui == Python.None {
         gui = args[0].kernel.gui
       }
-      print("hello world 6")
+      print("hello world 7")
       try ZMQInteractiveShell.enable_matplotlib.throwing
-        .dynamicallyCall(withArguments: `self`, gui)
+        .dynamicallyCall(withArguments: [`self`, gui])
       
 //       // TODO: Make these lines 80 characters
 //       print("checkpoint 1")
