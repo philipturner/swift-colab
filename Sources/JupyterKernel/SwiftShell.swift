8b2ee9692ea561581961411931dee945e0d54ddf
Philip Turner <philipturner.AR@gmail.com>
2022-06-04 10:20:39 -0400

    Update SwiftShell.swift

--- Sources/JupyterKernel/SwiftShell.swift
+++ Sources/JupyterKernel/SwiftShell.swift
@@ -84,8 +84,8 @@ fileprivate let SwiftShell = PythonClass(
       if gui == Python.None {
         gui = args[0].kernel.gui
       }
-      print("hello world 3")
-      try! ZMQInteractiveShell.enable_matplotlib.throwing
+      print("hello world 4")
+      try ZMQInteractiveShell.enable_matplotlib.throwing
         .dynamicallyCall(withArguments: `self`, gui)
       
 //       // TODO: Make these lines 80 characters
