cf3cc0e3b65c4ff12d144403e4480131ee8e72da
Philip Turner <philipturner.AR@gmail.com>
2022-06-17 14:08:26 -0700

    Update

--- Sources/JupyterKernel/SwiftKernel/DoExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/DoExecute.swift
@@ -83,6 +83,7 @@ func doExecute(code: String) throws -> PythonObject? {
       sendIOPubErrorMessage(message)
     } else {
       KernelContext.log("Had compiler error")
+      KernelContext.log(result.description)
       // There is no stdout, so it must be a compile error. Simply return the 
       // error without trying to get a stack trace.
       message = formatCompilerError(result.description)
