ee98667c970d8733c56c3ac02f2c5cb13879f61b
Philip Turner <philipturner.AR@gmail.com>
2022-08-11 17:12:34 -0700

    Update

--- Sources/JupyterKernel/SwiftShell.swift
+++ Sources/JupyterKernel/SwiftShell.swift
@@ -281,7 +281,7 @@ fileprivate func execute_send_multipart(_ input: PythonObject) -> Data {
 
 fileprivate func decode_send_multipart(_ input: Data) {
   let input_str = String(data: input, encoding: .utf8)!
-  let response = json.load(input_str.pythonObject)
+  let response = json.loads(input_str.pythonObject)
   precondition(response.count == 2, "Malformatted response.")
   precondition(
     response[0] == "send_request", 
